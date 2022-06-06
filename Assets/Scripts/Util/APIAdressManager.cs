a1b8f630d2b269343d26c05b75a9b89dd5f71b94
willtriti <willtriti03@naver.com>
2022-06-06 20:03:52 +0900

    Feat : Constants

--- Assets/Scripts/Util/APIAdressManager.cs
+++ Assets/Scripts/Util/APIAdressManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class APIAdressManager 
+public static class APIAdressManager 
 {
-    public const string REQUEST_LOGIN = "/"; 
+    public const string REQUEST_LOGIN = "/";
+    public const string REQUEST_GETTEST = "/";
 }

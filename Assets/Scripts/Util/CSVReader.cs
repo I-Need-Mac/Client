38a19de62dbdf742744f61b5dde7f17bdb9392ef
mongple <mongple92@gmail.com>
2022-06-17 00:12:41 +0900

    feat : Change CSV file Read format

--- Assets/Scripts/Util/CSVReader.cs
+++ Assets/Scripts/Util/CSVReader.cs
@@ -10,7 +10,7 @@ public class CSVReader
 	static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
 	static char[] TRIM_CHARS = { '\"' };
 
-	public static List<Dictionary<string, object>> Read(string file)
+	public static Dictionary<int, List<object>> Read(string file)
 	{
 		TextAsset data = Resources.Load(file) as TextAsset;
 
@@ -28,41 +28,59 @@ public class CSVReader
 			return null;
 		}
 
-		var list = new List<Dictionary<string, object>>();
+        Dictionary<int, List<object>> dataDic = new Dictionary<int, List<object>>();
+        string[] h = Regex.Split(lines[0], SPLIT_RE);
+        for (int i = 3; i < lines.Length; i++)
+        {
+            string[] val = Regex.Split(lines[i], SPLIT_RE);
 
-		var header = Regex.Split(lines[0], SPLIT_RE);
-		for (var i = 4; i < lines.Length; i++)
-		{
-			var values = Regex.Split(lines[i], SPLIT_RE);
-			
-			if (values.Length == 0 || values[0] == "")
-			{
-				continue;
-			}
+            List<object> _dataList = new List<object>();
+            for ( int j = 1; j<val.Length; j++)
+            {
+                _dataList.Add(val[j]);
+            }
 
-			var entry = new Dictionary<string, object>();
+            dataDic.Add(int.Parse(val[0]), _dataList);
+        }
 
-			for (var j = 0; j < header.Length && j < values.Length; j++)
-			{
-				string value = values[j].TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
-				object finalvalue = value;
-				int n;
-				float f;
+        return dataDic;
 
-				if (int.TryParse(value, out n))
-				{
-					finalvalue = n;
-				}
-				else if (float.TryParse(value, out f))
-				{
-					finalvalue = f;
-				}
+        //var list = new List<Dictionary<string, object>>();
 
-				entry[header[j]] = finalvalue;
-			}
+        //var header = Regex.Split(lines[0], SPLIT_RE);
+        //for (var i = 4; i < lines.Length; i++)
+        //{
+        //    var values = Regex.Split(lines[i], SPLIT_RE);
 
-			list.Add(entry);
-		}
-		return list;
-	}
+        //    if (values.Length == 0 || values[0] == "")
+        //    {
+        //        continue;
+        //    }
+
+        //    var entry = new Dictionary<string, object>();
+
+        //    for (var j = 0; j < header.Length && j < values.Length; j++)
+        //    {
+        //        string value = values[j].TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+        //        object finalvalue = value;
+        //        int n;
+        //        float f;
+
+        //        if (int.TryParse(value, out n))
+        //        {
+        //            finalvalue = n;
+        //        }
+        //        else if (float.TryParse(value, out f))
+        //        {
+        //            finalvalue = f;
+        //        }
+
+        //        entry[header[j]] = finalvalue;
+        //    }
+
+        //    list.Add(entry);
+        //}
+
+        //return list;
+    }
 }

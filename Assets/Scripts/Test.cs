76bda95022f9dedf346d8eb77365cc1b934d5fa8
mongple <mongple92@gmail.com>
2022-06-17 00:24:44 +0900

    Feat : Change CSV Reader

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,18 +7,18 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<Dictionary<string, object>> test = CSVReader.Read("CharacterStat");
+        Dictionary<int, List<object>> test = CSVReader.Read("CharacterStat");
 
         if (test != null)
         {
-            for (int i = 0; i < test.Count; i++)
-            {
-                Debug.LogFormat("=============================================");
-                Debug.LogFormat("CharacterId : {0}", test[i]["CharacterId"]);
-                Debug.LogFormat("Character_Name : {0}", test[i]["Character_Name"]);
-                Debug.LogFormat("CriRatio : {0}", test[i]["CriRatio"]);
-                Debug.LogFormat("=============================================");
-            }
+            //for (int i = 0; i < test.Count; i++)
+            //{
+            //    Debug.LogFormat("=============================================");
+            //    Debug.LogFormat("CharacterId : {0}", test[i]["CharacterId"]);
+            //    Debug.LogFormat("Character_Name : {0}", test[i]["Character_Name"]);
+            //    Debug.LogFormat("CriRatio : {0}", test[i]["CriRatio"]);
+            //    Debug.LogFormat("=============================================");
+            //}
         }
     }
 }

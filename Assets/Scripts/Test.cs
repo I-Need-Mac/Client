beab512b02ac0281ae27e65cee009252c360b9e7
mongple <mongple92@gmail.com>
2022-06-17 20:30:16 +0900

    Feat : Add CSV Reader

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,18 +7,19 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Dictionary<int, List<object>> test = CSVReader.Read("CharacterStat");
+        List<Dictionary<string, object>> test = CSVReader.Read("CharacterStat");
+        Dictionary<int, List<object>> csvRead = CSVReader.FileRead("CharacterStat");
 
         if (test != null)
         {
-            //for (int i = 0; i < test.Count; i++)
-            //{
-            //    Debug.LogFormat("=============================================");
-            //    Debug.LogFormat("CharacterId : {0}", test[i]["CharacterId"]);
-            //    Debug.LogFormat("Character_Name : {0}", test[i]["Character_Name"]);
-            //    Debug.LogFormat("CriRatio : {0}", test[i]["CriRatio"]);
-            //    Debug.LogFormat("=============================================");
-            //}
+            for (int i = 0; i < test.Count; i++)
+            {
+                Debug.LogFormat("=============================================");
+                Debug.LogFormat("CharacterId : {0}", test[i]["CharacterId"]);
+                Debug.LogFormat("Character_Name : {0}", test[i]["Character_Name"]);
+                Debug.LogFormat("CriRatio : {0}", test[i]["CriRatio"]);
+                Debug.LogFormat("=============================================");
+            }
         }
     }
 }
--- Assets/Scripts/Util/CSVReader.cs
+++ Assets/Scripts/Util/CSVReader.cs
@@ -10,7 +10,7 @@ public class CSVReader
 	static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
 	static char[] TRIM_CHARS = { '\"' };
 
-	public static Dictionary<int, List<object>> Read(string file)
+	public static List<Dictionary<string, object>> Read(string file)
 	{
 		TextAsset data = Resources.Load(file) as TextAsset;
 
@@ -28,6 +28,63 @@ public class CSVReader
 			return null;
 		}
 
+        var list = new List<Dictionary<string, object>>();
+
+        var header = Regex.Split(lines[0], SPLIT_RE);
+        for (var i = 4; i < lines.Length; i++)
+        {
+            var values = Regex.Split(lines[i], SPLIT_RE);
+
+            if (values.Length == 0 || values[0] == "")
+            {
+                continue;
+            }
+
+            var entry = new Dictionary<string, object>();
+
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j].TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+
+                entry[header[j]] = finalvalue;
+            }
+
+            list.Add(entry);
+        }
+
+        return list;
+    }
+
+    public static Dictionary<int, List<object>> FileRead(string file )
+    {
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        if (data == null)
+        {
+            Debug.LogErrorFormat("[Read] {0} 파일을 찾을 수 없습니다.", file);
+            return null;
+        }
+
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 3)
+        {
+            Debug.LogErrorFormat("[Read] 해당 파일의 내용이 부족합니다.");
+            return null;
+        }
+
         Dictionary<int, List<object>> dataDic = new Dictionary<int, List<object>>();
         string[] h = Regex.Split(lines[0], SPLIT_RE);
         for (int i = 3; i < lines.Length; i++)
@@ -35,7 +92,7 @@ public class CSVReader
             string[] val = Regex.Split(lines[i], SPLIT_RE);
 
             List<object> _dataList = new List<object>();
-            for ( int j = 1; j<val.Length; j++)
+            for (int j = 1; j < val.Length; j++)
             {
                 _dataList.Add(val[j]);
             }
@@ -44,43 +101,5 @@ public class CSVReader
         }
 
         return dataDic;
-
-        //var list = new List<Dictionary<string, object>>();
-
-        //var header = Regex.Split(lines[0], SPLIT_RE);
-        //for (var i = 4; i < lines.Length; i++)
-        //{
-        //    var values = Regex.Split(lines[i], SPLIT_RE);
-
-        //    if (values.Length == 0 || values[0] == "")
-        //    {
-        //        continue;
-        //    }
-
-        //    var entry = new Dictionary<string, object>();
-
-        //    for (var j = 0; j < header.Length && j < values.Length; j++)
-        //    {
-        //        string value = values[j].TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
-        //        object finalvalue = value;
-        //        int n;
-        //        float f;
-
-        //        if (int.TryParse(value, out n))
-        //        {
-        //            finalvalue = n;
-        //        }
-        //        else if (float.TryParse(value, out f))
-        //        {
-        //            finalvalue = f;
-        //        }
-
-        //        entry[header[j]] = finalvalue;
-        //    }
-
-        //    list.Add(entry);
-        //}
-
-        //return list;
     }
 }

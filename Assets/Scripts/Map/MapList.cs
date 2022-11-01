4c4b4704161c78a7326d40cac17cf9931eeeecdb
jiuseo665 <114589707+jiuseo665@users.noreply.github.com>
2022-11-01 22:14:37 +0900

    Add files via upload

---
+++ Assets/Scripts/Map/MapList.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MapName     
+{
+    Map1,
+    Map2,
+    Map3
+}
+
+public class MainUI : MonoBehaviour
+{
+
+}

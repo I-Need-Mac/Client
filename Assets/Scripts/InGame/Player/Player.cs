c7fb857a3bea3020fdec8ea4b6a142e20b850a09
TEN3T <esther78944@gmail.com>
2022-11-04 20:44:12 +0900

    Fix: 정해진 코딩 스타일에 맞춰 전면 수정

--- Assets/Scripts/InGame/Player/Player.cs
+++ Assets/Scripts/InGame/Player/Player.cs
@@ -2,20 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int Speed;
-    private Rigidbody2D Rigid;
-    private Vector3 Dir;
+    [SerializeField] private int moveSpeed;
 
-    private void Start()
+    private Rigidbody2D playerRigidbody;
+    private Vector3 playerDirection;
+
+    public PlayerData playerData { get; private set; } = new PlayerData();
+
+    private void Awake()
     {
-        Rigid = GetComponent<Rigidbody2D>();
-        Dir = Vector3.zero;
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerDirection = Vector3.zero;
     }
 
     /*
      *키보드 입력이랑 움직이는 부분은 안정성을 위해 분리시킴
      *Update -> 키보드 input
-     *FixedUpdate -> 움직임 구현
+     *FixedUpdate -> movement
      */
     private void Update()
     {
@@ -24,19 +27,20 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-
         Move();
     }
 
     private void KeyDir()
     {
-        Dir.x = Input.GetAxis("Horizontal");    //left, right
-        Dir.y = Input.GetAxis("Vertical");      //up, down
+        //left, right
+        playerDirection.x = Input.GetAxis("Horizontal");
+        //up, down
+        playerDirection.y = Input.GetAxis("Vertical");
     }
 
     private void Move()
     {
-        Rigid.MovePosition((Vector3)Rigid.position + (Dir * Speed * Time.fixedDeltaTime));
+        playerRigidbody.MovePosition((Vector3)playerRigidbody.position + (playerDirection * moveSpeed * Time.fixedDeltaTime));
     }
 
 }
--- Assets/Scripts/InGame/Player/PlayerData.cs
+++ Assets/Scripts/InGame/Player/PlayerData.cs
@@ -1,20 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerData
 {
-    public string CharacterName { get; private set; }  //캐릭터 이름
-    public int HP { get; private set; }                 //체력
-    public int Attack { get; private set; }             //공격력
-    public int CriRatio { get; private set; }          //크리티컬 확률
-    public int CriDamage { get; private set; }         //크리티컬 데미지
-    public int CoolDown { get; private set; }          //쿨타임 감소량
-    public int HPRegen { get; private set; }           //체젠량
-    public int Shield { get; private set; }             //쉴드 개수
-    public int ProjectileAdd { get; private set; }     //투사체 증가 개수
-    public int MoveSpeed { get; private set; }         //이동 속도
-    public int GetItemRange { get; private set; }     //아이템 획득 범위
+    public string characterName { get; private set; }  //캐릭터 이름
+    public int hp { get; private set; }                 //체력
+    public int attack { get; private set; }             //공격력
+    public int criRatio { get; private set; }          //크리티컬 확률
+    public int criDamage { get; private set; }         //크리티컬 데미지
+    public int coolDown { get; private set; }          //쿨타임 감소량
+    public int hpRegen { get; private set; }           //체젠량
+    public int shield { get; private set; }             //쉴드 개수
+    public int projectileAdd { get; private set; }     //투사체 증가 개수
+    public int moveSpeed { get; private set; }         //이동 속도
+    public int getItemRange { get; private set; }     //아이템 획득 범위
     //public int ultimateID { get; private set; }
     //public int skillID { get; private set; }
 
@@ -22,17 +19,17 @@ public class PlayerData
     //public SkillData skillData;
 
     //Property setter를 사용할 경우 get이 같이 참조되어 setter를 따로 생성
-    public void SetCharacterName(string CharacterName) { this.CharacterName = CharacterName; }
-    public void SetHp(int HP) { this.HP = HP; }
-    public void SetAttack(int Attack) { this.Attack = Attack; }
-    public void SetCriRatio(int CriRatio) { this.CriRatio = CriRatio; }
-    public void SetCriDamage(int CriDamage) { this.CriDamage = CriDamage; }
-    public void SetCoolDown(int CoolDown) { this.CoolDown = CoolDown; }
-    public void SetHPRegen(int HPRegen) { this.HPRegen = HPRegen; }
-    public void SetShield(int Shield) { this.Shield = Shield; }
-    public void SetProjectileAdd(int ProjectileAdd) { this.ProjectileAdd = ProjectileAdd; }
-    public void SetMoveSpeed(int MoveSpeed) { this.MoveSpeed = MoveSpeed; }
-    public void SetGetItemRange(int GetItemRange) { this.GetItemRange = GetItemRange; }
+    public void SetCharacterName(string characterName) { this.characterName = characterName; }
+    public void SetHp(int hp) { this.hp = hp; }
+    public void SetAttack(int attack) { this.attack = attack; }
+    public void SetCriRatio(int criRatio) { this.criRatio = criRatio; }
+    public void SetCriDamage(int criDamage) { this.criDamage = criDamage; }
+    public void SetCoolDown(int coolDown) { this.coolDown = coolDown; }
+    public void SetHpRegen(int hpRegen) { this.hpRegen = hpRegen; }
+    public void SetShield(int shield) { this.shield = shield; }
+    public void SetProjectileAdd(int projectileAdd) { this.projectileAdd = projectileAdd; }
+    public void SetMoveSpeed(int moveSpeed) { this.moveSpeed = moveSpeed; }
+    public void SetGetItemRange(int getItemRange) { this.getItemRange = getItemRange; }
     //public void SetUltimateID(int ultimateID) { this.ultimateID = ultimateID; }
     //public void SetskillID(int skillID) { this.skillID = skillID; }
     //public void SetUltimateData(SkillData ultimateData) { this.ultimateData = ultimateData; }

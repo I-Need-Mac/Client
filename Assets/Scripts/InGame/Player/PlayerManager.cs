194739f88d3fac4e03021375b640c7838a6c490e
TEN3T <esther78944@gmail.com>
2022-11-06 20:46:46 +0900

    Fix: maxHp 변수 제거 (Player쪽에서 관리하도록 로직 수정), 싱글톤을 상속받게 수정

--- Assets/Scripts/InGame/Player/PlayerManager.cs
+++ Assets/Scripts/InGame/Player/PlayerManager.cs
@@ -1,44 +1,51 @@
+using BFM;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+//싱글톤 사용
+public class PlayerManager : SingletonBehaviour<PlayerManager>
 {
     [SerializeField] private Player player;
-    [SerializeField] private string testCharacterId; //테스트용 필드
+    [SerializeField] private string testCharacterId; //테스트용 필드: 캐릭터id
+    [SerializeField] private Vector2 spawnPos; //테스트용 필드: 스폰위치
 
     private PlayerData playerData; //플레이어의 데이터를 가지는 객체
-    private int maxHp; //최대 체력
 
     private void Awake()
     {
         playerData = player.playerData;
-        SetPlayerStatus(testCharacterId);
+        PlayerSetting(FindCharacter(testCharacterId));
+        SetPlayerSpawnPos(spawnPos);
+    }
+
+    //플레이어 스폰 위치
+    private void SetPlayerSpawnPos(Vector2 newPos)
+    {
+        player.transform.position = newPos;
     }
 
     //캐릭터에 스탯 부여
-    private void SetPlayerStatus(string characterId)
+    private void PlayerSetting(Dictionary<string, object> characterData)
     {
-        Dictionary<string, object> findRow = FindCharacter(characterId);
-        if(findRow == null)
+        if (characterData == null)
         {
             //없는 캐릭터일 경우 에러메시지 송출
             Debug.LogError("존재하지 않는 캐릭터입니다");
             return;
         }
 
-        playerData.SetCharacterName(findRow["Character_Name"].ToString());
-        playerData.SetHp(Convert.ToInt32(findRow["HP"]));
-        maxHp = playerData.hp;
-        playerData.SetAttack(Convert.ToInt32(findRow["Attack"]));
-        playerData.SetCriRatio(Convert.ToInt32(findRow["CriRatio"]));
-        playerData.SetCriDamage(Convert.ToInt32(findRow["CriDamage"]));
-        playerData.SetCoolDown(Convert.ToInt32(findRow["CoolDown"]));
-        playerData.SetHpRegen(Convert.ToInt32(findRow["HPRegen"]));
-        playerData.SetShield(Convert.ToInt32(findRow["Shield"]));
-        playerData.SetProjectileAdd(Convert.ToInt32(findRow["ProjectileAdd"]));
-        playerData.SetMoveSpeed(Convert.ToInt32(findRow["MoveSpeed"]));
-        playerData.SetGetItemRange(Convert.ToInt32(findRow["GetItemRange"]));
+        playerData.SetCharacterName(characterData["Character_Name"].ToString());
+        playerData.SetHp(Convert.ToInt32(characterData["HP"]));
+        playerData.SetAttack(Convert.ToInt32(characterData["Attack"]));
+        playerData.SetCriRatio(Convert.ToInt32(characterData["CriRatio"]));
+        playerData.SetCriDamage(Convert.ToInt32(characterData["CriDamage"]));
+        playerData.SetCoolDown(Convert.ToInt32(characterData["CoolDown"]));
+        playerData.SetHpRegen(Convert.ToInt32(characterData["HPRegen"]));
+        playerData.SetShield(Convert.ToInt32(characterData["Shield"]));
+        playerData.SetProjectileAdd(Convert.ToInt32(characterData["ProjectileAdd"]));
+        playerData.SetMoveSpeed(Convert.ToInt32(characterData["MoveSpeed"]));
+        playerData.SetGetItemRange(Convert.ToInt32(characterData["GetItemRange"]));
 
         //Debug.Log("캐릭터 이름: " + playerData.characterName);
         //Debug.Log("캐릭터 체력: " + playerData.hp);
@@ -68,5 +75,5 @@ public class PlayerManager : MonoBehaviour
         //찾는 캐릭터가 없을 경우 null 리턴
         return null;
     }
-    
+
 }

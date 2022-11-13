d95adab958a5b198c6066105d05ad750de1e33f4
TEN3T <esther78944@gmail.com>
2022-11-13 23:14:58 +0900

    Fix: 기존 Enum 구조에서 관련성에 따라 분리 및 이동

---
+++ Assets/Scripts/InGame/Projectile/ProjectileConstant.cs
@@ -0,0 +1,11 @@
+
+public enum PROJECTILE_TYPE
+{
+    STRAIGHT,
+    PROTECT,
+    SATELLITE,
+    DROP,
+    BOOMERANG,
+    RANGE,
+    BOOM,
+}
---
+++ Assets/Scripts/InGame/Skill/SkillConstant.cs
@@ -0,0 +1,30 @@
+
+namespace SKILLCONSTANT
+{
+    public enum SKILL_EFFECT
+    {
+        STUN,
+        SLOW,
+        NUCK_BACK,
+        BOOM,
+    }
+
+    public enum SKILL_TARGET
+    {
+        MELEE,
+        FRONT,
+        BACK,
+        TOP,
+        BOTTOM,
+        RANDOM_DROP,
+        RANDOM,
+        NEAR,
+        BOSS,
+    }
+
+    public enum CALC_DAMAGE_TYPE
+    {
+        PLUS,
+        MULTI,
+    }
+}

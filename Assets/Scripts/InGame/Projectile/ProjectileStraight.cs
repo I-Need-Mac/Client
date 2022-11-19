a1dad84e613c708cc4bac9139a92675639d6e33e
TEN3T <esther78944@gmail.com>
2022-11-19 22:51:23 +0900

    Feat: Stright Projectile 발사 구현

--- Assets/Scripts/InGame/Projectile/ProjectileStraight.cs
+++ Assets/Scripts/InGame/Projectile/ProjectileStraight.cs
@@ -1,8 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ProjectileStraight : Projectile
 {
+    Vector3 movePos = Vector3.zero;
+
+    protected override void Move()
+    {
+        movePos.Normalize();
+        transform.Translate(movePos * Time.fixedDeltaTime * 7);
+    }
+
+    public override void Fire(Transform caster, Vector3 direction)
+    {
+        transform.position = caster.position;
+        movePos = direction;
+        gameObject.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
 
 }

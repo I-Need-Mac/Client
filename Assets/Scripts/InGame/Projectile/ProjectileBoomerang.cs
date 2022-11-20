bfd0488c0016fa27ad4ba3dc488cfb37019b453c
TEN3T <esther78944@gmail.com>
2022-11-21 03:02:39 +0900

    Feat: 부메랑 구현

--- Assets/Scripts/InGame/Projectile/ProjectileBoomerang.cs
+++ Assets/Scripts/InGame/Projectile/ProjectileBoomerang.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class ProjectileBoomerang : Projectile
 {
-    
+    bool isReturn = false;
+    int attackDistanceTemp = 7;
+
+    protected override void Move()
+    {
+        if (!isReturn)
+        {
+            transform.Translate(5 * Time.fixedDeltaTime * direction);
+            if(Vector2.Distance(transform.position, direction * attackDistanceTemp) <= 2f)
+            {
+                DebugManager.Instance.PrintDebug("돌아가!");
+                direction *= -1;
+                isReturn = true;
+            }
+        }
+        transform.Translate(5 * Time.fixedDeltaTime * direction);
+    }
+
+    public override void Fire(Transform caster, Vector3 pos)
+    {
+        transform.position = caster.position;
+        isReturn = false;
+        direction = pos;
+        direction.Normalize();
+        gameObject.SetActive(true);
+    }
+
 }

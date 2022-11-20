c1ae9332932d2aa4c748781b9982c39538b3930f
TEN3T <esther78944@gmail.com>
2022-11-21 03:03:37 +0900

    Feat: Satellite 구현

--- Assets/Scripts/InGame/Projectile/ProjectileSatellite.cs
+++ Assets/Scripts/InGame/Projectile/ProjectileSatellite.cs
@@ -1,8 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ProjectileSatellite : Projectile
 {
-    
+    private Transform caster;
+    private int count = 3;
+    private float tempSpeed = 120f;
+    private float radius = 4f;
+    private float angle = 0f;
+
+    protected override void Move()
+    {
+        transform.position = caster.position + (transform.position - caster.position).normalized * radius;
+        transform.RotateAround(caster.position, Vector3.forward, tempSpeed * Time.fixedDeltaTime);
+        //angle += tempSpeed * Time.fixedDeltaTime;
+        //Vector3 offset = Quaternion.Euler(0f, angle, 0f) * new Vector3(radius, 0f, 0f);
+        //transform.position = new Vector3(caster.position.x, caster.position.y, caster.position.z) + offset;
+    }
+
+    public override void Fire(Transform caster, Vector3 pos)
+    {
+        transform.position = caster.position * 10;
+        this.caster = caster;
+        gameObject.SetActive(true);
+    }
+
 }

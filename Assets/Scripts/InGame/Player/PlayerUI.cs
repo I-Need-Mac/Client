95cd07a7dbb2e9c88efbae3382c50fbc686cd12b
TEN3T <esther78944@gmail.com>
2022-12-04 19:54:56 +0900

    Feat: 임시 Status UI 구현

--- Assets/Scripts/InGame/Player/PlayerUI.cs
+++ Assets/Scripts/InGame/Player/PlayerUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,15 +8,25 @@ public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private Player player;
+    [SerializeField] private Text playerLevel;
+    [SerializeField] private Text playerName;
 
-    private void Awake()
+    private void Start()
     {
+        DebugManager.Instance.PrintDebug(player.playerData.hp);
         slider.maxValue = player.playerData.hp;
-        slider.value = player.playerData.hp;
+        slider.value = slider.maxValue;
+        playerLevel.text += "1";
+        playerName.text = player.playerData.characterName;
     }
 
     private void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.K))
+        {
+            player.ModifyHp(0, 5);
+            DebugManager.Instance.PrintDebug(slider.value);
+        }
+        slider.value = player.hp;
     }
 }

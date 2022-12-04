22fac792c673cbdc59029e7de1bc1b86fa6687f5
jiuseo665 <jiuseo665@gmail.com>
2022-12-04 19:37:03 +0900

    Fix : Map 불러오는 함수 / 프리팹 불러오는 함수로 나눔

--- Assets/Scripts/Util/PrefabLoader.cs
+++ Assets/Scripts/Util/PrefabLoader.cs
@@ -6,12 +6,15 @@ using System;
 public class PrefabLoader : SingleTon<PrefabLoader>
 {
     private Dictionary<string, GameObject> cachedPrefab = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> cachedMap = new Dictionary<string, GameObject>();
 
-    const string MAP_PREFAB_HOME = "MapPrefabs/";
+    const string MAP_HOME = "Maps/";
+    const string PREFAB_HOME = "Prefabs/";
 
     public PrefabLoader()
     {
-        cachedPrefab.Add("Dummy", Resources.Load(MAP_PREFAB_HOME + "Dummy", typeof(GameObject)) as GameObject);
+        cachedPrefab.Add("Dummy", Resources.Load(PREFAB_HOME + "Dummy", typeof(GameObject)) as GameObject);
+        cachedMap.Add("Dummy", Resources.Load(MAP_HOME + "Dummy", typeof(GameObject)) as GameObject);
     }
 
 
@@ -27,13 +30,37 @@ public class PrefabLoader : SingleTon<PrefabLoader>
         {
             DebugManager.Instance.PrintDebug("[PrefabLoader] New Prefab : " + path);
 
-            returnPrefab = Resources.Load(MAP_PREFAB_HOME + path, typeof(GameObject)) as GameObject;
+            returnPrefab = Resources.Load(PREFAB_HOME + path, typeof(GameObject)) as GameObject;
             if (returnPrefab == null)
             {
                 DebugManager.Instance.PrintDebug("[PrefabLoader] Dummy Prefab : 경로 이상");
                 returnPrefab = cachedPrefab["Dummy"];
             }
             else { cachedPrefab.Add(path, returnPrefab); }
+
+        }
+        return returnPrefab;
+    }
+
+    public GameObject LoadMapToGameObject(string path)
+    {
+        GameObject returnPrefab;
+        if (cachedMap.ContainsKey(path))
+        {
+            DebugManager.Instance.PrintDebug("[PrefabLoader] Cached Map : " + path);
+            returnPrefab = cachedMap[path];
+        }
+        else
+        {
+            DebugManager.Instance.PrintDebug("[PrefabLoader] New Map : " + path);
+
+            returnPrefab = Resources.Load(MAP_HOME + path, typeof(GameObject)) as GameObject;
+            if (returnPrefab == null)
+            {
+                DebugManager.Instance.PrintDebug("[PrefabLoader] Dummy Prefab : 경로 이상");
+                returnPrefab = cachedMap["Dummy"];
+            }
+            else { cachedMap.Add(path, returnPrefab); }
 
         }
         return returnPrefab;

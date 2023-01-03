1f37b6f8b45c076d06fe96483f7b24eb999fd9dd
TEN3T <esther78944@gmail.com>
2023-01-03 10:09:08 +0900

    Fix: Awake -> Start로 수정

--- Assets/Scripts/InGame/Camera/CameraManager.cs
+++ Assets/Scripts/InGame/Camera/CameraManager.cs
@@ -8,12 +8,17 @@ public class CameraManager : SingletonBehaviour<CameraManager>
 
     protected override void Awake()
     {
+    }
+
+    private void Start()
+    {
         virtualCamera = transform.Find("Cinemachine").GetComponent<CinemachineVirtualCamera>();
         ConfinerSetting("Floor");
     }
 
     private void ConfinerSetting(string mapName)
     {
+        DebugManager.Instance.PrintDebug("test: "+GameObject.Find(mapName));
         virtualCamera.GetComponent<CinemachineConfiner>().m_BoundingShape2D =
             GameObject.Find(mapName).GetComponent<CompositeCollider2D>();
     }

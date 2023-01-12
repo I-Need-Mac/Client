348dc79b3ad4a4ff3f3dd40af13fd2efee96823a
TEN3T <esther78944@gmail.com>
2023-01-12 18:44:38 +0900

    Feat: 캐릭터 이름으로 스파인 로드하는 기능 구현

--- Assets/Scripts/SpineAnimation/SpineManager.cs
+++ Assets/Scripts/SpineAnimation/SpineManager.cs
@@ -1,19 +1,38 @@
 using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpineManager : MonoBehaviour
 {
+    const string SPINE_HOME = "Arts/SpineSample/";
+
+    [SerializeField] private string characterName;
     [SerializeField] private AnimationReferenceAsset[] animationClips;
     [SerializeField] private SkeletonAnimation skeletonAnimation;
 
     public AnimationConstant animationState { get; set; }
+
     private string currentAnimation;
+
+    private void Awake()
+    {
+        animationState = AnimationConstant.IDLE;
+    }
 
     private void Start()
     {
-        animationState = AnimationConstant.IDLE;
+        SpineSetting();
+    }
+
+    private void SpineSetting()
+    {
+        string path = SPINE_HOME + characterName + "/";
+        SkeletonAnimation character = transform.Find("Character").GetComponent<SkeletonAnimation>();
+        character.skeletonDataAsset = Resources.Load<SkeletonDataAsset>(path + "SkeletonData");
+        animationClips[0] = Resources.Load<AnimationReferenceAsset>(path + "ReferenceAssets/IDLE");
+        animationClips[1] = Resources.Load<AnimationReferenceAsset>(path + "ReferenceAssets/RUN");
     }
 
     private void PlayAnimation(AnimationReferenceAsset clip, bool loop, float timeScale)
@@ -28,14 +47,13 @@ public class SpineManager : MonoBehaviour
 
     public void SetCurrentAnimation()
     {
+        skeletonAnimation.timeScale = 1f;
         switch (animationState)
         {
             case AnimationConstant.IDLE:
-                skeletonAnimation.timeScale = 0f;
-                //PlayAnimation(animationClips[(int)PlayerAnimationConstant.IDLE], true, 1f);
+                PlayAnimation(animationClips[(int)AnimationConstant.IDLE], true, 1f);
                 break;
             case AnimationConstant.WALK:
-                skeletonAnimation.timeScale = 1f;
                 PlayAnimation(animationClips[(int)AnimationConstant.WALK], true, 1f);
                 break;
         }
@@ -46,14 +64,14 @@ public class SpineManager : MonoBehaviour
         float x = skeletonAnimation.transform.localScale.x;
         if (direction.x < 0)
         {
-            if (x > 0)
+            if (x < 0)
             {
                 x *= -1;
             }
         }
         else if (direction.x > 0)
         {
-            if (x < 0)
+            if (x > 0)
             {
                 x *= -1;
             }

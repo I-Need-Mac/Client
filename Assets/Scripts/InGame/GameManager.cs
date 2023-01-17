45059759c3ee4d1e89bb64aa547fc275481ff6e1
TEN3T <esther78944@gmail.com>
2023-01-17 21:30:35 +0900

    Feat: Map & Player Load 분리

--- Assets/Scripts/InGame/GameManager.cs
+++ Assets/Scripts/InGame/GameManager.cs
@@ -17,22 +17,33 @@ public class GameManager : MonoBehaviour
     [SerializeField] private string map;
     [SerializeField] private string player;
 
+    private float defaultScale;
+
     private void Start()
     {
+        defaultScale = float.Parse(Convert.ToString(CSVReader.Read("BattleConfig", "ImageMultiple", "ConfigValue")));
         Spawn();
     }
 
     private void Spawn()
     {
+        MapLoad();
+        PlayerLoad();
+    }
+
+    private void MapLoad()
+    {
         string name = LoadMapManager.Instance.SceneNumberToMapName(10101);
         GameObject temp = LoadMapManager.Instance.LoadMapNameToMapObject(name);
         GameObject map = Instantiate(temp, transform);
         map.gameObject.transform.SetParent(transform.Find("MapGeneratePos").transform);
-        float defaultScale = float.Parse(Convert.ToString(CSVReader.Read("BattleConfig", "ImageMultiple", "ConfigValue")));
         map.gameObject.transform.localScale = new Vector3(defaultScale, defaultScale, defaultScale);
         map.transform.position = new Vector3(map.transform.position.x, map.transform.position.y, 1);
         map.gameObject.SetActive(true);
+    }
 
+    private void PlayerLoad()
+    {
         Player p = playerPoolManager.SpawnPlayer(transform.Find("PlayerSpawnPos").transform);
         p.gameObject.transform.localScale = new Vector3(defaultScale, defaultScale, defaultScale);
         p.transform.position = new Vector3(p.transform.position.x, p.transform.position.y, 0);

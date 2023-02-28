ac920b04e9142e086986b0ce338d8db3c9cf8e25
TEN3T <esther78944@gmail.com>
2023-03-01 02:37:44 +0900

    Feat: Spine Animator
    +Init function

--- Assets/Scripts/SpineAnimation/SpineAnimatorManager.cs
+++ Assets/Scripts/SpineAnimation/SpineAnimatorManager.cs
@@ -10,11 +10,33 @@ public class SpineAnimatorManager : MonoBehaviour
 {
     private const string IMAGE_PATH = "/Animation/Spine/";
 
+    private SkeletonMecanim skeletonMecanim;
+    private Animator animator;
+
+    private string path;
+
     public AnimationConstant animationState { get; set; }
+
+    private void Start()
+    {
+        Init();
+    }
 
     private void Init()
     {
+        path = PathFinding();
+        skeletonMecanim = transform.Find("Character").GetComponent<SkeletonMecanim>();
+        skeletonMecanim.skeletonDataAsset = ResourcesManager.Load<SkeletonDataAsset>(path + "SkeletonData");
+        skeletonMecanim.Initialize(true);
 
+        animator = transform.Find("Character").GetComponent<Animator>();
+        animator.runtimeAnimatorController = ResourcesManager.Load<RuntimeAnimatorController>(path + "Controller");
+        DebugManager.Instance.PrintDebug("#######" + ResourcesManager.Load<SkeletonDataAsset>(path + "SkeletonData"));
+    }
+
+    private string PathFinding()
+    {
+        return GetComponent<Monster>().monsterData.monsterImage + IMAGE_PATH;
     }
 
 }

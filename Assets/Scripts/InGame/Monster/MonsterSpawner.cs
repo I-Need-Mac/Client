b0b687e65a78d47f0208d3d9c9da32aae10b6446
TEN3T <esther78944@gmail.com>
2023-03-16 17:43:18 +0900

    Feat: Monster Spawner Update
    +남은 몬스터들을 큐에 저장한 후 불러오는 방식 구현
    +Face, Back, Round 구현
    +성능 개선 (불필요하게 반복되는 코드 정리)

--- Assets/Scripts/InGame/Monster/MonsterSpawner.cs
+++ Assets/Scripts/InGame/Monster/MonsterSpawner.cs
@@ -1,8 +1,6 @@
 using BFM;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class MonsterSpawner : SingletonBehaviour<MonsterSpawner>
@@ -14,11 +12,24 @@ public class MonsterSpawner : SingletonBehaviour<MonsterSpawner>
     private int spawnAmount;
     private int spawnCount;
     private Dictionary<int, MonsterPool> spawner;
-    private Queue<int> remainMonsters;
+    private Queue<RemainMonster> remainMonsters;
 
-    private Dictionary<string, Dictionary<string, object>> sponeData;
-    private int spawnIndex;
+    private Dictionary<string, Dictionary<string, object>> sponeTable;
+    private Dictionary<string, object> sponeData;
+    private int spawnId;
     private int currentSpawnTime;
+
+    private struct RemainMonster
+    {
+        public int id;
+        public SponeMobLocation location;
+
+        public RemainMonster(int id, SponeMobLocation location)
+        {
+            this.id = id;
+            this.location = location;
+        }
+    }
     
     protected override void Awake()
     {
@@ -26,9 +37,12 @@ public class MonsterSpawner : SingletonBehaviour<MonsterSpawner>
         spawnAmount = Convert.ToInt32(CSVReader.Read("StageTable", stageId.ToString(), "LimitAmount"));
         spawnCount = 0;
         spawner = new Dictionary<int, MonsterPool>();
-        remainMonsters = new Queue<int>();
-        sponeData = CSVReader.Read(CSVReader.Read("StageTable", stageId.ToString(), "MonsterSponeID").ToString());
-        spawnIndex = 1;
+        remainMonsters = new Queue<RemainMonster>();
+
+        sponeTable = CSVReader.Read(CSVReader.Read("StageTable", stageId.ToString(), "MonsterSponeID").ToString());
+        spawnId = 1;
+        sponeData = sponeTable[spawnId.ToString()];
+        currentSpawnTime = int.Parse(sponeData["SponeTime"].ToString());
     }
 
     private void Start()
@@ -38,16 +52,10 @@ public class MonsterSpawner : SingletonBehaviour<MonsterSpawner>
 
     private void Update()
     {
-        if (sponeData.ContainsKey(spawnIndex.ToString()))
+        if (Timer.Instance.currentTime >= currentSpawnTime)
         {
-            Dictionary<string, object> data = sponeData[spawnIndex.ToString()];
-            currentSpawnTime = int.Parse(data["SponeTime"].ToString());
-            if (Timer.Instance.currentTime >= currentSpawnTime)
-            {
-                Spawn(data);
-            }
+            Spawn(sponeData);
         }
-        
     }
 
     private void SpawnerInit()
@@ -102,16 +110,63 @@ public class MonsterSpawner : SingletonBehaviour<MonsterSpawner>
 
     private void Spawn(Dictionary<string, object> data)
     {
-        currentSpawnTime = int.Parse(data["SponeTime"].ToString());
-        
-        if (spawnCount < spawnAmount)
+        SponeMobLocation location = (SponeMobLocation)Enum.Parse(typeof(SponeMobLocation), data["SponeMobLocation"].ToString().ToUpper());
+        int mobAmount = int.Parse(data["SponeMobAmount"].ToString());
+        int mobId = int.Parse(data["SponeMobID"].ToString());
+        Player player = GameManager.Instance.player;
+
+        for (int i = 0; i < mobAmount; i++)
         {
-            for (int i = 0; i < int.Parse(data["SponeMobAmount"].ToString()); i++)
+            if (spawnCount < spawnAmount)
             {
-                SponeMobLocation location = (SponeMobLocation)Enum.Parse(typeof(SponeMobLocation), data["SponeMobLocation"].ToString().ToUpper());
-                Monster monster = SpawnMonster(int.Parse(data["SponeMobID"].ToString()), CameraManager.Instance.RandomPosInGrid(location));
+                Monster monster;
+
+                if (remainMonsters.Count > 0)
+                {
+                    RemainMonster remainMonster = remainMonsters.Dequeue();
+                    monster = SpawnMonster(remainMonster.id, CameraManager.Instance.RandomPosInGrid(remainMonster.location));
+                }
+                else
+                {
+                    if (location == SponeMobLocation.ROUND)
+                    {
+                        location = (SponeMobLocation)UnityEngine.Random.Range(0, (int)location);
+                    }
+                    else if (location == SponeMobLocation.FACE)
+                    {
+                        if (player.lookDirection.x < 0)
+                        {
+                            location = SponeMobLocation.LEFT;
+                        }
+                        else
+                        {
+                            location = SponeMobLocation.RIGHT;
+                        }
+                    }
+                    else if (location == SponeMobLocation.BACK)
+                    {
+                        if (player.lookDirection.x < 0)
+                        {
+                            location = SponeMobLocation.RIGHT;
+                        }
+                        else
+                        {
+                            location = SponeMobLocation.LEFT;
+                        }
+                    }
+                    monster = SpawnMonster(mobId, CameraManager.Instance.RandomPosInGrid(location));
+                }
+                ++spawnCount;
             }
-            ++spawnIndex;
+            else
+            {
+                remainMonsters.Enqueue(new RemainMonster(mobId, location));
+            }
+        }
+        if (sponeTable.ContainsKey((++spawnId).ToString()))
+        {
+            sponeData = sponeTable[spawnId.ToString()];
+            currentSpawnTime = int.Parse(sponeData["SponeTime"].ToString());
         }
     }
 

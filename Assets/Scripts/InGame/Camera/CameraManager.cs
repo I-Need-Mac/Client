005bacb8397c43f448093cf5d80f3046142833d6
TEN3T <esther78944@gmail.com>
2023-03-16 17:41:59 +0900

    Feat: Camera Manager Update
    +구조 개선

--- Assets/Scripts/InGame/Camera/CameraManager.cs
+++ Assets/Scripts/InGame/Camera/CameraManager.cs
@@ -38,82 +38,71 @@ public class CameraManager : SingletonBehaviour<CameraManager>
         Vector2 camPoint = cam.ScreenToWorldPoint(new Vector2(cam.orthographicSize * Screen.width / Screen.height, cam.orthographicSize));
         Vector2 weight = new Vector2((cam.orthographicSize * Screen.width / Screen.height * 2) / 3.0f, (cam.orthographicSize * 2) / 3.0f);
 
-        Vector2 pos;
+        Vector3 pos = Vector3.zero;
         switch (location)
         {
             case SponeMobLocation.TOPLEFT:
                 pos = new Vector2(Random.Range(camPoint.x - extra, camPoint.x), Random.Range(-camPoint.y, -camPoint.y + extra));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
+                    pos = new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
                 }
-                return pos;
+                break;
             case SponeMobLocation.TOP:
                 pos = new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(-camPoint.y, -camPoint.y + extra));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
+                    pos = new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
                 }
-                return pos;
+                break;
             case SponeMobLocation.TOPRIGHT:
                 pos = new Vector2(Random.Range(-camPoint.x, -camPoint.x + extra), Random.Range(-camPoint.y, -camPoint.y + extra));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
+                    pos = new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y + weight.y * 2, -camPoint.y));
                 }
-                return pos;
+                break;
             case SponeMobLocation.LEFT:
                 pos = new Vector2(Random.Range(camPoint.x - extra, camPoint.x), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
+                    pos = new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
                 }
-                return pos;
+                break;
             case SponeMobLocation.RIGHT:
                 pos = new Vector2(Random.Range(-camPoint.x, -camPoint.x + extra), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
+                    pos = new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y + weight.y, camPoint.y + weight.y * 2));
                 }
-                return pos;
+                break;
             case SponeMobLocation.BOTTOMLEFT:
                 pos = new Vector2(Random.Range(camPoint.x - extra, camPoint.x), Random.Range(camPoint.y - extra, camPoint.y));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y, camPoint.y + weight.y));
+                    pos = new Vector2(Random.Range(camPoint.x, camPoint.x + weight.x), Random.Range(camPoint.y, camPoint.y + weight.y));
                 }
-                return pos;
+                break;
             case SponeMobLocation.BOTTOM:
                 pos = new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(camPoint.y - extra, camPoint.y));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(camPoint.y, camPoint.y + weight.y));
+                    pos = new Vector2(Random.Range(camPoint.x + weight.x, camPoint.x + weight.x * 2), Random.Range(camPoint.y, camPoint.y + weight.y));
                 }
-                return pos;
+                break;
             case SponeMobLocation.BOTTOMRIGHT:
                 pos = new Vector2(Random.Range(-camPoint.x, -camPoint.x + extra), Random.Range(camPoint.y - extra, camPoint.y));
                 if (tileMap.GetTile(tileMap.WorldToCell(pos)) == null)
                 {
-                    DebugManager.Instance.PrintDebug("escape!");
-                    return new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y, camPoint.y + weight.y));
+                    pos = new Vector2(Random.Range(camPoint.x + weight.x * 2, -camPoint.x), Random.Range(camPoint.y, camPoint.y + weight.y));
                 }
-                return pos;
-            case SponeMobLocation.ROUND:
-                return Vector2.zero;
-            case SponeMobLocation.FACE:
-                return Vector2.zero;
-            case SponeMobLocation.BACK:
-                return Vector2.zero;
+                break;
             default:
-                return Vector2.zero;
+                pos = Vector2.zero;
+                break;
         }
+        pos.z = (int)LayerConstant.MONSTER;
+        return pos;
     }
 
     #endregion

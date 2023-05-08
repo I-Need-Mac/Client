3f3c4beaf273d96df663c40df622c9bac2dfefd7
TEN3T <esther78944@gmail.com>
2023-05-08 17:40:25 +0900

    Feat: DeiMisericordia
    +전체 데미지 처리 제외 구현

---
+++ Assets/Scripts/InGame/Skill/PlayerSkill/DeiMisericordia.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeiMisericordia : Skill
+{
+    public DeiMisericordia(int skillId, Transform shooter) : base(skillId, shooter) { }
+
+    public override void Init()
+    {
+        Projectile projectile = SkillManager.Instance.SpawnProjectile(skillData, shooter);
+        projectile.transform.localScale = Vector2.zero;
+        projectile.CollisionRadius(skillData.splashRange);
+        projectile.CollisionPower(false);
+        projectiles.Add(projectile);
+    }
+
+    public override IEnumerator Activation()
+    {
+        if (!skillData.isEffect)
+        {
+            yield return coolTime;
+        }
+
+        while (true)
+        {
+            if (skillData.splashRange < 10)
+            {
+                for (int i = 0; i < skillData.projectileCount; i++)
+                {
+                    projectiles[0].transform.localScale = Vector2.one;
+                    projectiles[0].CollisionPower(true);
+                    yield return duration;
+                    projectiles[0].CollisionPower(false);
+                    projectiles[0].transform.localScale = Vector2.zero;
+                }
+            }
+            else    //일정범위 초과시 맵 전체 타격
+            {
+                foreach (Monster monster in MonsterSpawner.Instance.monsters)
+                {
+                    //데미지 처리
+                }
+            }
+            
+            yield return coolTime;
+        }
+    }
+}

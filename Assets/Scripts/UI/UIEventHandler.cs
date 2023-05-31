f67fceb2dff41fa8e673db2f07ad582d97aede30
mongpleee92 <mongpleee92@gmail.com>
2023-06-01 06:51:45 +0900

    Feat : 호버처리
    Feat : 호버처리

--- Assets/Scripts/UI/UIEventHandler.cs
+++ Assets/Scripts/UI/UIEventHandler.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 // UI이벤트 핸들러
-public class UIEventHandler : MonoBehaviour, IPointerClickHandler
+public class UIEventHandler : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Action<PointerEventData> OnClickHandler = null;
+    public Action<PointerEventData> OnEnterHandler = null;
+    public Action<PointerEventData> OnExitHandler = null;
 
     // 오브젝트에서 포인터를 누르고 동일한 오브젝트에서 뗄 때 호출
     public void OnPointerClick(PointerEventData eventData)
     {
-        // 우선 클릭 이벤트만 처리하도록 합니다.
-        // 추후 mouse over, up, down등 이벤트를 추가하도록 합니다.
         if (OnClickHandler != null)
         {
             OnClickHandler.Invoke(eventData);
@@ -21,4 +21,29 @@ public class UIEventHandler : MonoBehaviour, IPointerClickHandler
         }
     }
 
+    // 오브젝트에서 포인터를 ui위에 올렸을 때 호출
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(OnEnterHandler != null)
+        {
+            OnClickHandler.Invoke(eventData);
+            if (GetComponent<SoundRequesterBtn>() != null)
+            {
+                GetComponent<SoundRequesterBtn>().ChangeSituation(SoundSituation.SOUNDSITUATION.HOVER);
+            }
+        }
+    }
+
+    // 오브젝트에서 포인터를 ui위에서 내렸을 때 호출 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (OnExitHandler != null)
+        {
+            OnExitHandler.Invoke(eventData);
+            if (GetComponent<SoundRequesterBtn>() != null)
+            {
+                GetComponent<SoundRequesterBtn>().ChangeSituation(SoundSituation.SOUNDSITUATION.HOVER);
+            }
+        }
+    }
 }

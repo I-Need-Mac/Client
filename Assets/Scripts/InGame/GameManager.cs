f167c49cfe28f6017763c280bae0b67cedafaac4
TEN3T <esther78944@gmail.com>
2023-06-13 14:38:21 +0900

    Feat: Object spawn point
    +맵 내부에 찍은 위치로 캐릭터가 스폰되게 변경
    +PrefabLoader -> ResourcesManager 교체

--- Assets/Scripts/InGame/GameManager.cs
+++ Assets/Scripts/InGame/GameManager.cs
@@ -72,19 +72,19 @@ public class GameManager : SingletonBehaviour<GameManager>
 
     private void MapLoad(int mapId)
     {
-        string name = LoadMapManager.Instance.SceneNumberToMapName(mapId);
-        GameObject mapPrefab = LoadMapManager.Instance.LoadMapNameToMapObject(name);
-        map = Instantiate(mapPrefab, transform);
-        tileMap = map.transform.Find("Map").transform.Find("Floor").GetComponent<Tilemap>();
-        map.transform.SetParent(transform.Find("MapGeneratePos").transform);
-        map.transform.localScale = new Vector3(defaultScale, defaultScale, defaultScale);
-        map.SetActive(true);
+        string mapName = CSVReader.Read("StageTable", mapId.ToString(), "MapID").ToString();
+        GameObject map = ResourcesManager.Load<GameObject>("Maps/" + mapName);
+        this.map = Instantiate(map, transform);
+        tileMap = this.map.transform.Find("Map").Find("Floor").GetComponent<Tilemap>();
+        this.map.transform.localScale = Vector3.one * defaultScale;
+        this.map.SetActive(true);
     }
 
     private void PlayerLoad(int playerId)
     {
-        player = Instantiate(ResourcesManager.Load<Player>(CSVReader.Read("CharacterTable", playerId.ToString(), "CharacterPrefabPath").ToString()), transform.Find("PlayerSpawnPos").transform);
+        player = Instantiate(ResourcesManager.Load<Player>(CSVReader.Read("CharacterTable", playerId.ToString(), "CharacterPrefabPath").ToString()), transform);
         player.transform.localScale = Vector3.one * defaultCharScale;
+        player.transform.localPosition = this.map.transform.Find("SpawnPoint").Find("PlayerPoint").localPosition;
         player.gameObject.SetActive(true);
     }
 

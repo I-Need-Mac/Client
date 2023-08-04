0b0bbf50d72ae6ad1e7a8b4e0505883365551ae0
TEN3T <esther78944@gmail.com>
2023-08-04 13:16:01 +0900

    Fix: GwiGi bug fix
    +발사간격 적용
    +낙하 가속도 추가

--- Assets/Scripts/InGame/Skill/PlayerSkill/GwiGi.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/GwiGi.cs
@@ -20,12 +20,11 @@ public class GwiGi : ActiveSkill
             for (int i = 0; i < skillData.projectileCount; i++)
             {
                 Projectile projectile = SkillManager.Instance.SpawnProjectile(skillData, shooter);
-                projectile.SetAlpha(0.0f);
                 projectile.CollisionPower(false);
                 projectile.transform.localPosition = Vector2.up * skillData.attackDistance;
                 projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
-                projectile.SetAlpha(1.0f);
                 SkillManager.Instance.CoroutineStarter(Move(projectile));
+                yield return intervalTime;
             }
 
             yield return PlayerUI.Instance.skillBoxUi.boxIcons[skillNum].Dimmed(skillData.coolTime);
@@ -36,28 +35,29 @@ public class GwiGi : ActiveSkill
     {
         projectile.CollisionPower(true);
         float angle = 0.0f;
+        float weight = 0.0f;
         if (Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance).x >= 0)
         {
             do
             {
-                angle -= Time.fixedDeltaTime * skillData.speed;
+                weight += 0.002f;
+                angle -= Time.fixedDeltaTime * skillData.speed + weight;
                 projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
                 yield return frame;
-                DebugManager.Instance.PrintDebug("[GwiGi]: " + projectile.transform.localEulerAngles.z);
             } while (projectile.transform.localEulerAngles.z > 240.0f);
         }
         else
         {
             while (projectile.transform.localEulerAngles.z < 100.0f)
             {
-                angle += Time.fixedDeltaTime * skillData.speed;
+                weight += 0.001f;
+                angle += Time.fixedDeltaTime * skillData.speed + weight;
                 projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
                 yield return frame;
-                DebugManager.Instance.PrintDebug("[GwiGi]: " + projectile.transform.localEulerAngles.z);
             }
         }
-        projectile.CollisionPower(false);
-        projectile.SetAlpha(0.0f);
+
+        SkillManager.Instance.DeSpawnProjectile(projectile);
     }
 
 }

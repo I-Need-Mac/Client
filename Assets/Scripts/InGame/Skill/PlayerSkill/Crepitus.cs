c71869836ba841a54dfe632b54d65ea49e2ebf5b
TEN3T <esther78944@gmail.com>
2023-08-06 18:24:35 +0900

    Fix: Crepitus bug fix
    +콜라이더 추가
    +로직 개선

--- Assets/Scripts/InGame/Skill/PlayerSkill/Crepitus.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Crepitus.cs
@@ -9,23 +9,9 @@ public class Crepitus : ActiveSkill
 
     public Crepitus(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
 
-    //public override void Init()
-    //{
-    //    tick = new WaitForSeconds(0.5f);
-    //    size = skillData.attackDistance >= 10 ? 10 : skillData.attackDistance;
-
-    //    for (int i = 0; i < skillData.projectileCount; i++)
-    //    {
-    //        Projectile projectile = SkillManager.Instance.SpawnProjectile(skillData);
-    //        projectile.SetAlpha(0.0f);
-    //        projectile.CollisionPower(false);
-    //        projectiles.Add(projectile);
-    //    }
-    //}
-
     public override IEnumerator Activation()
     {
-        tick = new WaitForSeconds(0.5f);
+        tick = new WaitForSeconds(0.25f);
         size = skillData.attackDistance >= 10 ? 10 : skillData.attackDistance;
 
         if (!skillData.isEffect)
@@ -38,17 +24,9 @@ public class Crepitus : ActiveSkill
             for (int i = 0; i < skillData.projectileCount; i++)
             {
                 Projectile projectile = SkillManager.Instance.SpawnProjectile(skillData);
-                projectile.SetAlpha(0.0f);
-                projectile.CollisionPower(false);
                 SkillManager.Instance.CoroutineStarter(Boom(projectile));
                 yield return intervalTime;
             }
-
-            //foreach (Projectile projectile in projectiles)
-            //{
-            //    SkillManager.Instance.CoroutineStarter(Boom(projectile));
-            //    yield return intervalTime;
-            //}
 
             yield return PlayerUI.Instance.skillBoxUi.boxIcons[skillNum].Dimmed(skillData.coolTime);
         } while (skillData.coolTime > 0.0f);
@@ -57,8 +35,10 @@ public class Crepitus : ActiveSkill
     private IEnumerator Boom(Projectile projectile)
     {
         projectile.transform.localPosition = Scanner.GetTarget(SKILLCONSTANT.SKILL_TARGET.RANDOM, shooter, size);
+        projectile.CollisionPower(true);
         projectile.SetAlpha(1.0f);
         yield return tick;
+        projectile.CollisionPower(false);
         projectile.SetAlpha(0.0f);
     }
 }

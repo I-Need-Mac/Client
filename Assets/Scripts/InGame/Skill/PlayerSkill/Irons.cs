9d0908830b6807c237583af0f6df0edd4c5b0f6f
TEN3T <esther78944@gmail.com>
2023-08-16 22:02:46 +0900

    Feat: Irons는 더이상 아니다 옹졸하지

--- Assets/Scripts/InGame/Skill/PlayerSkill/Irons.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Irons.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Irons : ActiveSkill
@@ -8,45 +10,53 @@ public class Irons : ActiveSkill
 
     public override IEnumerator Activation()
     {
-        int weight = 0;
+        //int weight = 0;
 
         if (!skillData.isEffect)
         {
             yield return PlayerUI.Instance.skillBoxUi.boxIcons[skillNum].Dimmed(skillData.coolTime);
         }
 
+        List<int> angles = new List<int>();
         do
         {
             for (int i = 0; i < skillData.projectileCount; i++)
             {
+                angles.Add(i);
+            }
+            angles.FisherYateShuffle();
+
+            for (int i = 0; i < skillData.projectileCount; i++)
+            {
                 ProjectileStraight projectile = (ProjectileStraight)SkillManager.Instance.SpawnProjectile(skillData);
                 projectile.transform.localPosition = shooter.position;
-                Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
-                pos -= (Vector2)shooter.position;
-                float angle = 0.0f;
-                if (UnityEngine.Random.Range(0, 100) < 20)
-                {
-                    pos *= -1;
-                }
-                else
-                {
-                    if (weight < (skillData.projectileCount / 5))
-                    {
-                        ++weight;
-                    }
-                    else
-                    {
-                        DebugManager.Instance.PrintDebug("[TEST]: ");
-                        weight = 0;
-                        pos *= -1;
-                        angle += UnityEngine.Random.Range(1, 10) * 10;
-                    }
-                }
-                angle += Quaternion.FromToRotation(Vector3.up, pos).eulerAngles.z;
-                projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
+                //Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
+                //pos -= (Vector2)shooter.position;
+                //float angle = 0.0f;
+                //if (UnityEngine.Random.Range(0, 100) < 20)
+                //{
+                //    pos *= -1;
+                //}
+                //else
+                //{
+                //    if (weight < (skillData.projectileCount / 5))
+                //    {
+                //        ++weight;
+                //    }
+                //    else
+                //    {
+                //        DebugManager.Instance.PrintDebug("[TEST]: ");
+                //        weight = 0;
+                //        pos *= -1;
+                //        angle += UnityEngine.Random.Range(1, 10) * 10;
+                //    }
+                //}
+                //angle += Quaternion.FromToRotation(Vector3.up, pos).eulerAngles.z;
+                projectile.transform.rotation = Quaternion.Euler(0, 0, angles[i] * 360.0f / skillData.projectileCount);
                 yield return intervalTime;
             }
 
+            angles.Clear();
             yield return PlayerUI.Instance.skillBoxUi.boxIcons[skillNum].Dimmed(skillData.coolTime);
         } while (skillData.coolTime > 0.0f);
 

8eff6a3f48473e055c563ee221c468a7f3745d4d
TEN3T <esther78944@gmail.com>
2023-09-02 21:53:00 +0900

    Feat: MobStatue cool time apply

--- Assets/Scripts/InGame/FieldStructure/MobStatue.cs
+++ Assets/Scripts/InGame/FieldStructure/MobStatue.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class MobStatue : FieldStructure
 {
+    private SpriteRenderer spriteRenderer;
+
     protected override void Awake()
     {
         base.Awake();
 
+        spriteRenderer = top.GetComponent<SpriteRenderer>();
         this.hp = float.Parse(this.fieldStructureData.gimmickParam[0]);
     }
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!spriteRenderer.enabled)
+        {
+            return;
+        }
+
         if (collision.gameObject.TryGetComponent(out Projectile projectile))
         {
             this.hp -= projectile.skillData.damage;
             if (this.hp <= 0)
             {
-                ItemManager.Instance.DropItems(this.fieldStructureData.gimmickParam[1], transform);
-                gameObject.SetActive(false);
+                //ItemManager.Instance.DropItems(this.fieldStructureData.gimmickParam[1], transform);
+                //gameObject.SetActive(false);
+                StartCoroutine(Activation());
             }
         }
+    }
+
+    private IEnumerator Activation()
+    {
+        ItemManager.Instance.DropItems(this.fieldStructureData.gimmickParam[1], transform);
+        spriteRenderer.enabled = false;
+        top.enabled = false;
+        yield return new WaitForSeconds(this.fieldStructureData.coolTime);
+        spriteRenderer.enabled = true;
+        top.enabled = this.fieldStructureData.topIsPassable;
     }
 }

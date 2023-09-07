45736b73fdd09b1cd32ff3f5668c15ba76763ff0
TEN3T <esther78944@gmail.com>
2023-09-07 21:16:20 +0900

    Feat: Create gimmick beam skeleton code

---
+++ Assets/Scripts/InGame/FieldStructure/Beams/BeamWidth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamWidth : MonoBehaviour, IBeam
+{
+    public void SetBeam(float n)
+    {
+        throw new System.NotImplementedException();
+    }
+}

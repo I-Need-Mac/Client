fe109d377fbc9daec7e43e90ede37a917387f700
cuptae <106813678+cuptae@users.noreply.github.com>
2023-10-24 18:08:52 +0900

    Feat: Rank 텍스트 로컬라이즈

--- Assets/Scripts/UI/MainUI/UI_RankBoard.cs
+++ Assets/Scripts/UI/MainUI/UI_RankBoard.cs
@@ -14,30 +14,44 @@ public class UI_RankBoard : UI_Popup
     }
     [SerializeField]
     TextMeshProUGUI titleText;
+    [Header("-----RankData-----")]
     [SerializeField]
     TextMeshProUGUI rankText;
     [SerializeField]
-    TextMeshProUGUI nickNameText;
+    TextMeshProUGUI nicknameText;
     [SerializeField]
     TextMeshProUGUI progressText;
     [SerializeField]
     TextMeshProUGUI characterText;
     [SerializeField]
     TextMeshProUGUI timeText;
+    [Header("-----PlayerInfo-----")]
     [SerializeField]
-    TextMeshProUGUI playerText;
-
+    TextMeshProUGUI playernicknameText;
+    [SerializeField]
+    TextMeshProUGUI cleartimeText;
+    [SerializeField]
+    TextMeshProUGUI playercleartimeText;
+    [SerializeField]
+    TextMeshProUGUI levelText;
     private void Start()
     {
         Bind<Image>(typeof(Images));
 
         Array imageValue = Enum.GetValues(typeof(Images));
 
-        // 버튼 이벤트 등록
         for (int i = 0; i < imageValue.Length; i++)
         {
             BindUIEvent(GetImage(i).gameObject, (PointerEventData data) => { OnClickImage(data); }, Define.UIEvent.Click);
         }
+        titleText.text = LocalizeManager.Instance.GetText("UI_RankingBoard");
+        cleartimeText.text = LocalizeManager.Instance.GetText("UI_ClearTime_Text");
+        playercleartimeText.text = LocalizeManager.Instance.GetText("UI_ClearTime");
+        rankText.text = LocalizeManager.Instance.GetText("UI_Rank_Rank");
+        nicknameText.text = LocalizeManager.Instance.GetText("UI_Rank_Name");
+        progressText.text = LocalizeManager.Instance.GetText("UI_Rank_Progress");
+        characterText.text = LocalizeManager.Instance.GetText("UI_Rank_Character");
+        timeText.text = LocalizeManager.Instance.GetText("UI_Rank_Time");
     }
 
     public void OnClickImage(PointerEventData data)

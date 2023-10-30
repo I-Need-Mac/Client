9289b689d2501d0d699bd50145971d3917336efb
cuptae <106813678+cuptae@users.noreply.github.com>
2023-10-30 21:16:01 +0900

    fix: ParkSung 일부 기능 수정

--- Assets/Scripts/InGame/Skill/PlayerSkill/ParkSung.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/ParkSung.cs
@@ -1,3 +1,5 @@
+using SKILLCONSTANT;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,35 +9,30 @@ public class ParkSung : ActiveSkill
 {
     List<Projectile> projectiles = new List<Projectile>();
     public ParkSung(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
-    List<Transform> Alltargets = new List<Transform>();
+    List<Transform> allTargets = new List<Transform>();
     public override IEnumerator Activation()
-    {       
-        Alltargets = Scanner.RangeTarget(shooter, skillData.attackDistance, 6);
-        List<Transform> Skilltargets = Alltargets
+    {
+        allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER);
+        List<Transform> skillTargets = allTargets
         .OrderBy(monster => Vector2.Distance(shooter.position, monster.position))
         .Take(skillData.projectileCount)
         .ToList();
         
-        foreach (Transform target in Skilltargets)
+        foreach (Transform target in skillTargets)
         {
             if (target != null)
             {
                 Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
-                projectile.transform.parent = target;
-                projectile.transform.localPosition = new Vector3(0, 1, 0);
-                projectile.transform.localScale = Vector3.one;
-                target.GetComponent<Monster>().SkillEffectActivation(SKILLCONSTANT.SKILL_EFFECT.STUN, skillData.duration);
-                target.GetComponent<Monster>().Hit(skillData.damage);
+                projectile.transform.position = target.position;
                 projectiles.Add(projectile);
             }
         }
         Debug.Log("박승 발동");
-        yield return new WaitForSeconds(skillData.duration);
+        yield return new WaitForSeconds(int.Parse(skillData.skillEffectParam[0]));
         for(int i = 0; i< projectiles.Count; i++)
         {
             SkillManager.Instance.DeSpawnProjectile(projectiles[i]);
         }
-        yield return intervalTime;
     }
 
 }

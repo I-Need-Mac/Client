9e4b326939d4102a66f2c2cf4b3cb9a8df34c038
TEN3T <esther78944@gmail.com>
2023-10-30 20:38:51 +0900

    Feat: Ending window localizing

--- Assets/Scripts/InGame/InGameUI/GameOverUI.cs
+++ Assets/Scripts/InGame/InGameUI/GameOverUI.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    private Button reStartBtn;
+    [SerializeField] private bool isWin = true;
+    private Button endBtn;
     private Image fadeBox;
+    private TextMeshProUGUI getItemText;
 
     private void Awake()
     {
         fadeBox = transform.Find("FadeBox").GetComponent<Image>();
         fadeBox.gameObject.SetActive(false);
 
-        reStartBtn = GetComponentInChildren<Button>();
-        reStartBtn.onClick.AddListener(ReStart);
+        endBtn = GetComponentInChildren<Button>();
+        endBtn.onClick.AddListener(ReStart);
+        if (isWin)
+        {
+            endBtn.GetComponentInChildren<TextMeshProUGUI>().text = LocalizeManager.Instance.GetText("UI_Result_Confirm");
+        }
+        else
+        {
+            endBtn.GetComponentInChildren<TextMeshProUGUI>().text = LocalizeManager.Instance.GetText("UI_Result_Exit");
+        }
+
+        getItemText = transform.Find("GetItemText").GetComponent<TextMeshProUGUI>();
+        getItemText.text = LocalizeManager.Instance.GetText("UI_Result_GetItem");
     }
 
     private void ReStart()
     {
         fadeBox.gameObject.SetActive(true);
-        reStartBtn.interactable = false;
+        endBtn.interactable = false;
         StartCoroutine(SceneFadeOut());
     }
 

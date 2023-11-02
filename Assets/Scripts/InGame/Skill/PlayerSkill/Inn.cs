4d95aabe94f68cc6d7d2415c8a1cf409efd1f09d
TEN3T <esther78944@gmail.com>
2023-11-03 01:58:28 +0900

    Feat: Inn skill optimize

--- Assets/Scripts/InGame/Skill/PlayerSkill/Inn.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Inn.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class Inn : ActiveSkill
 {
-    Projectile projectile;
+    //Projectile projectile;
+    //List<Transform> allTargets = new List<Transform>();
+    //public List<Transform> targets = new List<Transform>();
+    //변수는 제일 위쪽으로, 사용 범위 생각해서 접근제한자 붙히기
+    //미사용 변수는 제거
+    private List<Transform> allTargets = new List<Transform>();
+
     public Inn(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
-    List<Transform> allTargets = new List<Transform>();
-    public List<Transform> targets = new List<Transform>();
+    
     public override IEnumerator Activation()
     {
         allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER);
-        projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter);
+        Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter);
         foreach (Transform target in allTargets )
         {
-            target.GetComponent<Monster>().SkillEffectActivation(SKILLCONSTANT.SKILL_EFFECT.PULL,skillData.attackDistance);
-            target.GetComponent<Monster>().Hit(skillData.damage);
+            //target.GetComponent<Monster>().SkillEffectActivation(SKILLCONSTANT.SKILL_EFFECT.PULL,skillData.attackDistance);
+            //target.GetComponent<Monster>().Hit(skillData.damage);
+            if (target.TryGetComponent(out Monster monster))
+            {
+                monster.SkillEffectActivation(SKILLCONSTANT.SKILL_EFFECT.PULL, skillData.attackDistance);
+                monster.Hit(skillData.damage);
+            }
         }
         Debug.Log("인 발동");
-        yield return null;
+        yield return frame;
+
+        SkillManager.Instance.DeSpawnProjectile(projectile);
     }
     //IEnumerator ShrinkProjectile()
     //{

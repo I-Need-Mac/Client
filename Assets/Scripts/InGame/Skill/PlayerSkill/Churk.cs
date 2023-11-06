9fb27ee9339642ad9855c77405059adfe1277e08
cuptae <106813678+cuptae@users.noreply.github.com>
2023-11-07 03:10:57 +0900

    Feat: Churk projectileprefab with function

--- Assets/Scripts/InGame/Skill/PlayerSkill/Churk.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Churk.cs
@@ -6,19 +6,27 @@ public class Churk : ActiveSkill
 {
     private List<Transform> allTargets = new List<Transform>();
 
+    Projectile projectile;
     public Churk(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
     public override IEnumerator Activation()
     {
-        allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER);
-        foreach (Transform target in allTargets)
-        {
-            if (target.TryGetComponent(out Monster monster))
-            {
-                monster.SkillEffectActivation(SKILLCONSTANT.SKILL_EFFECT.KNOCKBACK, skillData.attackDistance);
-                monster.Hit(skillData.damage);
-            }
-        }
+        allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER,(int)LayerConstant.ITEM);
+        projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter);
         Debug.Log("척 발동");
+        yield return expansionProjectile();
+    }
+    IEnumerator expansionProjectile()
+    {
+        float initialScale = skillData.projectileSizeMulti;
+
+        while (initialScale < skillData.attackDistance * 2)
+        {
+            float newScale = Mathf.Max(0.0f, initialScale + 10.0f * Time.deltaTime);
+            projectile.transform.localScale = new Vector2(newScale, newScale);
+            yield return frame;
+            initialScale = newScale;
+        }
         yield return frame;
+        SkillManager.Instance.DeSpawnProjectile(projectile);       
     }
 }

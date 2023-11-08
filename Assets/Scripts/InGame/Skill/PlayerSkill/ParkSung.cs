47346df3e4a455338a5436c6e61fbe6c8c8006f3
cuptae <106813678+cuptae@users.noreply.github.com>
2023-11-08 17:05:03 +0900

    Feat: ParkSung projectile delete

--- Assets/Scripts/InGame/Skill/PlayerSkill/ParkSung.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/ParkSung.cs
@@ -8,40 +8,37 @@ using UnityEngine;
 
 public class ParkSung : ActiveSkill
 {
-    List<Projectile> projectiles = new List<Projectile>();
+    private List<Projectile> projectiles = new List<Projectile>();
+    private List<Transform> allTargets = new List<Transform>();
+    private List<Transform> targets = new List<Transform>();
+
     public ParkSung(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
-    List<Transform> allTargets = new List<Transform>();
-    public List<Transform> targets = new List<Transform>();
     public override IEnumerator Activation()
     {
-        //allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER);
-        //List<Transform> skillTargets = allTargets
-        //.OrderBy(monster => Vector2.Distance(shooter.position, monster.position))
-        //.Take(skillData.projectileCount)
-        //.ToList();
-
-        for (int i = 0; i < skillData.projectileCount; i++)
+        allTargets = Scanner.RangeTarget(shooter, skillData.attackDistance, (int)LayerConstant.MONSTER);
+        while (targets.Count<skillData.projectileCount)
         {
             Transform target = Scanner.GetTargetTransform(skillData.skillTarget, shooter, skillData.attackDistance);
-
             if (!targets.Contains(target))
             {
                 targets.Add(target);
-
-                if (targets.Count >= 5)
+                if (targets.Count>=allTargets.Count)
                 {
                     break;
                 }
             }
         }
-
-
+        
         foreach (Transform target in targets)
         {
             if (target != null)
             {
                 Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
                 projectile.transform.position = target.position+new Vector3(0,0.3f,0);
+                if (target.TryGetComponent(out Monster monster))
+                {
+                    monster.SkillEffectActivation(skillData.skillEffect[0], float.Parse(skillData.skillEffectParam[0]));
+                }
                 projectiles.Add(projectile);
             }
         }

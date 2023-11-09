f36ce82e450ec716feaf45fc13b4e7c752c49eb6
cuptae <106813678+cuptae@users.noreply.github.com>
2023-11-09 17:48:51 +0900

    Feat: JuckHwa Despawnprojectile update

--- Assets/Scripts/InGame/Skill/PlayerSkill/JuckHwa.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/JuckHwa.cs
@@ -6,42 +6,54 @@ public class JuckHwa : ActiveSkill
 {
     private List<Projectile> projectiles = new List<Projectile>();
     public JuckHwa(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
-
     public override IEnumerator Activation()
     {
-        float ignitionlTime = 0.1f;
-        float extinguishTime = 0.3f;
+        float ignitionlTime = 0.0f;
+
         float activateTime = 0.0f;
-        Debug.Log("적화 발동");
         while (activateTime < skillData.duration)
         {
-            //float currentTime=Time.time;
-            if (ignitionlTime<=0.0f)
+            if (ignitionlTime >= 0.2f)
             {
                 Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
                 projectile.transform.position = shooter.position;
                 projectiles.Add(projectile);
-                ignitionlTime = 0.2f;
+                ignitionlTime = 0.0f;
             }
             else
             {
-                ignitionlTime -= Time.fixedDeltaTime;
+                ignitionlTime += Time.fixedDeltaTime;
             }
-            if (extinguishTime<=0.0f)
-            {
-                if (projectiles.Count > 0)
-                {
-                    SkillManager.Instance.DeSpawnProjectile(projectiles[0]);
-                    projectiles.RemoveAt(0);
-                    extinguishTime = 0.3f;
-                }
-            }
-            else
-            {
-                extinguishTime -= Time.fixedDeltaTime;
-            }            
+
             activateTime += Time.fixedDeltaTime;
             yield return frame;
         }
+
+        foreach (var projectile in projectiles)
+        {
+            SkillManager.Instance.DeSpawnProjectile(projectile);
+        }
     }
+    //private IEnumerator ExtinguishProjectile()
+    //{
+    //    float time = 0.0f;
+    //    while (time < skillData.duration)
+    //    {
+    //        for (int i = 0; i < projectiles.Count; i++)
+    //        {
+    //            if (skillData.duration - time <= 0.5f)
+    //            {
+    //                projectiles[i].transform.localScale = originSize * (skillData.duration - time);
+    //            }
+    //        }
+    //        time += Time.fixedDeltaTime;
+    //        yield return frame;
+    //    }
+
+    //    for (int i = 0; i < projectiles.Count; i++)
+    //    {
+    //        SkillManager.Instance.DeSpawnProjectile(projectiles[i]);
+    //    }
+    //}
 }
+

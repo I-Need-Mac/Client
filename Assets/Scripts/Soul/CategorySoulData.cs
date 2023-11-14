96593c4eedded24bea06b9a44ceade95d764c667
TEN3T <esther78944@gmail.com>
2023-11-14 20:21:16 +0900

    Feat: SoulData, CategorySoulData update

--- Assets/Scripts/Soul/CategorySoulData.cs
+++ Assets/Scripts/Soul/CategorySoulData.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CategorySoulData
 {
-    public string categoryId { get; private set; }
-    public int order { get; private set; }
-    public string categoryName { get; private set; }
+    public string categoryId { get; private set; }      //대분류 혼의 아이디 값
+    public int order { get; private set; }              //대분류 혼의 화면 상에서 정렬 순서 낮 -> 높
+    public string categoryName { get; private set; }    //로컬라이즈 아이디
     //언락 조건 텍스트 아이디
-    public string image { get; private set; }
+    public string image { get; private set; }           //대분류 혼의 일러스트 패스
     //언락 조건 이넘 배열
     //언락 조건 파람 배열
-    public int soulCount { get; private set; }
+    public int soulCount { get; private set; }          //하위 혼 총 개수
 
     public void SetCategoryId(string categoryId) { this.categoryId = categoryId; }
     public void SetOrder(int order) { this.order = order; }
--- Assets/Scripts/Soul/SoulData.cs
+++ Assets/Scripts/Soul/SoulData.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SoulData
 {
-    public int soulId { get; private set; }
-    public string soulName { get; private set; }
-    public string soulExplain { get; private set; }
-    public string image { get; private set; }
-    //혼 효과 이넘 배열
-    //혼 효과 파람 배열
-    public string categorySoul { get; private set; }
-    public int colGroup { get; private set; }
-    public int orderInCol { get; private set; }
+    public int soulId { get; private set; }             //하위 혼 아이디 값
+    public string soulName { get; private set; }        //하위 혼 로컬라이즈 아이디
+    public string soulExplain { get; private set; }     //하위 혼 효과 로컬라이즈 아이디
+    public string image { get; private set; }           //하위 혼 이미지 패스
+    public List<SoulEffect> soulEffects { get; private set; }
+    public List<float> effectParams { get; private set; }
+    public string categorySoul { get; private set; }    //속해 있는 대분류 혼 아이디
+    public int colGroup { get; private set; }           //하위 혼 내 행 그룹
+    public int orderInCol { get; private set; }         //행 그룹 내 정렬 순서
     //언락 조건 이넘 배열
     //언락 조건 파람 배열
 
@@ -20,6 +20,8 @@ public class SoulData
     public void SetSoulName(string soulName) { this.soulName = soulName; }
     public void SetSoulExplain(string soulExplain) { this.soulExplain = soulExplain; }
     public void SetSoulImage(string image) { this.image = image; }
+    public void SetSoulEffect(List<SoulEffect> soulEffects) { this.soulEffects = soulEffects; }
+    public void SetEffectParams(List<float> effectParams) { this.effectParams = effectParams; }
     public void SetCategorySoul(string categorySoul) { this.categorySoul = categorySoul; }
     public void SetColGroup(int colGroup) { this.colGroup = colGroup; }
     public void SetOrderInCol(int orderInCol) { this.orderInCol = orderInCol; }

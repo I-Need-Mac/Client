ab697cf639e9e5c43ab8a31a8daa927c0b2ae946
cuptae <106813678+cuptae@users.noreply.github.com>
2023-11-20 08:20:59 +0900

    Feat: SunYang refactoring

--- Assets/Scripts/InGame/Skill/PlayerSkill/SunYang.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/SunYang.cs
@@ -1,56 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class SunYang : ActiveSkill
 {
     private Projectile[] projectiles;
 
+    private Vector2 look;
+
     public SunYang(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
+
     public override IEnumerator Activation()
     {
-        shooter = Scanner.GetTargetTransform(skillData.skillTarget, shooter, skillData.attackDistance);
-        projectiles = new Projectile[2];
+        projectiles = new Projectile[skillData.projectileCount];
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < projectiles.Length; i++)
         {
-            projectiles[i] = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter, false);
-            projectiles[i].transform.localPosition = (i == 0) ? Vector2.right*skillData.attackDistance : Vector2.right * -skillData.attackDistance;
+            projectiles[i] = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter);
+            if (shooter.TryGetComponent(out Player player))
+            {
+                look = player.lookDirection;
+            }
+            projectiles[i].transform.localPosition = look * skillData.attackDistance;
         }
+        Debug.Log($"{look.x},{look.y}");
 
         yield return Move();
-    }
 
+    }
     private IEnumerator Move()
     {
         Transform projectile1 = projectiles[0].transform;
         Transform projectile2 = projectiles[1].transform;
 
         float minDistance = 0.1f;
-        float angle = 0.0f;
+        float angle = 0f;
         float weight = 0.0f;
-
-        //while (Vector2.Distance(projectile1.position,projectile2.position)>minDistance)
-        //{
-        //    weight += 0.002f;
-        //    angle -= Time.fixedDeltaTime * skillData.speed + weight;
-        //    projectile1.transform.RotateAround(shooter.position, Vector3.forward, angle);
-        //    projectile2.transform.RotateAround(shooter.position, -Vector3.forward, angle);
-        //    yield return frame;
-        //}
-        float radius = skillData.attackDistance;
-
-        while (Vector2.Distance(projectile1.position, projectile2.position) > minDistance)
+        do
         {
             weight += 0.2f;
-            angle -= 1+ weight;
+            angle -= 1 + weight;
 
-            projectile1.position = shooter.position + Quaternion.Euler(0, 0, angle) * Vector2.right * radius;
-            projectile2.position = shooter.position + Quaternion.Euler(0, 0, -angle) * Vector2.left * radius;
+            projectile1.position = shooter.position + Quaternion.Euler(0, 0, angle) * look * skillData.attackDistance;
+            projectile2.position = shooter.position + Quaternion.Euler(0, 0, -angle) * -look * skillData.attackDistance;
 
             yield return frame;
-        }
-        for (int i=0; i<projectiles.Length;i++)
+        } while (Vector2.Distance(projectile1.position, projectile2.position) > minDistance);
+        for (int i = 0; i < projectiles.Length; i++)
         {
             SkillManager.Instance.DeSpawnProjectile(projectiles[i]);
         }

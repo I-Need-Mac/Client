290eaf1cf6abf869fee9417f49b095618dd7b775
TEN3T <esther78944@gmail.com>
2024-01-14 05:14:53 +0900

    Feat: GwiGi remake

--- Assets/Scripts/InGame/Skill/PlayerSkill/GwiGi.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/GwiGi.cs
@@ -10,11 +10,18 @@ public class GwiGi : ActiveSkill
     {
         shooter = Scanner.GetTargetTransform(skillData.skillTarget, shooter, skillData.attackDistance);
 
+        //for (int i = 0; i < skillData.projectileCount; i++)
+        //{
+        //    Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter, false);
+        //    projectile.transform.localPosition = Vector2.up * skillData.attackDistance;
+        //    projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+        //    SkillManager.Instance.CoroutineStarter(Move(projectile));
+        //    yield return intervalTime;
+        //}
+
         for (int i = 0; i < skillData.projectileCount; i++)
         {
-            Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter, false);
-            projectile.transform.localPosition = Vector2.up * skillData.attackDistance;
-            projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+            Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData, shooter, scaleType: SCALE_TYPE.HORIZON);
             SkillManager.Instance.CoroutineStarter(Move(projectile));
             yield return intervalTime;
         }
@@ -22,52 +29,33 @@ public class GwiGi : ActiveSkill
 
     private IEnumerator Move(Projectile projectile)
     {
-        float angle = 0.0f;
-        float weight = 0.0f;
-        if (shooter.TryGetComponent(out Player player))
-        {
-            if (player.lookDirection.x >= 0)
-            {
-                do
-                {
-                    weight += 0.002f;
-                    angle -= Time.fixedDeltaTime * skillData.speed + weight;
-                    projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
-                    yield return frame;
-                } while (projectile.transform.localEulerAngles.z > 240.0f);
-            }
-            else
-            {
-                while (projectile.transform.localEulerAngles.z < 100.0f)
-                {
-                    weight += 0.001f;
-                    angle += Time.fixedDeltaTime * skillData.speed + weight;
-                    projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
-                    yield return frame;
-                }
-            }
-        }
-
-        //if (Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance).x >= 0)
+        //float angle = 0.0f;
+        //float weight = 0.0f;
+        //if (shooter.TryGetComponent(out Player player))
         //{
-        //    do
+        //    if (player.lookDirection.x >= 0)
         //    {
-        //        weight += 0.002f;
-        //        angle -= Time.fixedDeltaTime * skillData.speed + weight;
-        //        projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
-        //        yield return frame;
-        //    } while (projectile.transform.localEulerAngles.z > 240.0f);
-        //}
-        //else
-        //{
-        //    while (projectile.transform.localEulerAngles.z < 100.0f)
+        //        do
+        //        {
+        //            weight += 0.002f;
+        //            angle -= Time.fixedDeltaTime * skillData.speed + weight;
+        //            projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
+        //            yield return frame;
+        //        } while (projectile.transform.localEulerAngles.z > 240.0f);
+        //    }
+        //    else
         //    {
-        //        weight += 0.001f;
-        //        angle += Time.fixedDeltaTime * skillData.speed + weight;
-        //        projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
-        //        yield return frame;
+        //        while (projectile.transform.localEulerAngles.z < 100.0f)
+        //        {
+        //            weight += 0.001f;
+        //            angle += Time.fixedDeltaTime * skillData.speed + weight;
+        //            projectile.transform.RotateAround(shooter.position, Vector3.forward, angle);
+        //            yield return frame;
+        //        }
         //    }
         //}
+
+        yield return duration;
 
         SkillManager.Instance.DeSpawnProjectile(projectile);
     }

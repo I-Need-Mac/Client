8f45ecf9350aa2e78ceffe7457d72469502131d7
TEN3T <esther78944@gmail.com>
2024-01-14 16:20:19 +0900

    Feat: AchievementData update & AchievementManager skeleton file add

--- Assets/Scripts/Achievement/AchievementData.cs
+++ Assets/Scripts/Achievement/AchievementData.cs
@@ -19,12 +19,12 @@ public class AchievementData
     #endregion
 
     #region Data
-    public Dictionary<int, int> monsterKillCount;                       //몬스터별 킬 횟수
-    public int levelUpCount;                                            //레벨업 횟수
-    public int statueDestroyCount;                                      //석상 파괴 개수
+    public Dictionary<int, int> monsterKillCount { get; private set; }                       //몬스터별 킬 횟수
+    public int levelUpCount { get; private set; }                                            //레벨업 횟수
+    public int statueDestroyCount { get; private set; }                                      //석상 파괴 개수
 
-    public Dictionary<int, int> stagePlayCount;                         //스테이지별 플레이 횟수
-    public int stagePlayTotalCount                                      //총 스테이지 플레이 횟수
+    public Dictionary<int, int> stagePlayCount { get; private set; }                         //스테이지별 플레이 횟수
+    public int stagePlayTotalCount                                                           //총 스테이지 플레이 횟수
     {
         get
         {
@@ -35,15 +35,15 @@ public class AchievementData
             }
             return total;
         }
-        set
+        private set
         {
 
         }
     }
 
-    public Dictionary<int, bool> isStageClear;                          //스테이지별 클리어 여부
-    public Dictionary<int, int> stageClearCount;                        //스테이지별 클리어 횟수
-    public int stageClearTotalCount                                     //총 스테이지 클리어 횟수
+    public Dictionary<int, bool> isStageClear { get; private set; }                          //스테이지별 클리어 여부
+    public Dictionary<int, int> stageClearCount { get; private set; }                        //스테이지별 클리어 횟수
+    public int stageClearTotalCount                                                          //총 스테이지 클리어 횟수
     {
         get
         {
@@ -54,19 +54,19 @@ public class AchievementData
             }
             return total;
         }
-        set
+        private set
         {
 
         }
     }
     
-    public int useKeyCount;                                             //열쇠 사용 횟수
-    public int acquireKeyCount;                                         //획득한 열쇠 개수
-    public int useBoxCount;                                             //박스 사용 횟수
-    public int acquireBoxCount;                                         //획득한 박스 개수
+    public int useKeyCount { get; private set; }                                             //열쇠 사용 횟수
+    public int acquireKeyCount { get; private set; }                                         //획득한 열쇠 개수
+    public int useBoxCount { get; private set; }                                             //박스 사용 횟수
+    public int acquireBoxCount { get; private set; }                                         //획득한 박스 개수
 
-    public Dictionary<int, int> skillLevelMaxCount;                     //스킬별 만렙 달성 횟수
-    public Dictionary<int, int> acquireSkillCount;                      //스킬별 획득 횟수
+    public Dictionary<int, int> skillLevelMaxCount { get; private set; }                     //스킬별 만렙 달성 횟수
+    public Dictionary<int, int> acquireSkillCount { get; private set; }                      //스킬별 획득 횟수
 
     //특정 레벨 이전 사망 체크 생각해봐야함
     #endregion

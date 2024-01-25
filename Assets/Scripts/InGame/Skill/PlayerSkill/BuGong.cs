46e60d410848edeb9efae491a1cc369bd9d1868a
TEN3T <esther78944@gmail.com>
2024-01-26 01:39:13 +0900

    Fix: Projectile direction bug

--- Assets/Scripts/InGame/Skill/PlayerSkill/BuGong.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/BuGong.cs
@@ -7,12 +7,14 @@ public class BuGong : ActiveSkill
     public BuGong(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
     public override IEnumerator Activation()
     {
-        ProjectileStraight projectile = SkillManager.Instance.SpawnProjectile<ProjectileStraight>(skillData);
-        projectile.transform.localPosition = shooter.position;
-        Vector2 targetPos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
-        Vector2 vec = (targetPos - (Vector2)shooter.position).normalized;
-        projectile.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg - 90.0f);
-        yield return intervalTime;      
+        for (int i = 0; i < skillData.projectileCount; i++)
+        {
+            ProjectileStraight projectile = SkillManager.Instance.SpawnProjectile<ProjectileStraight>(skillData);
+            projectile.transform.localPosition = shooter.position;
+            Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance) - (Vector2)shooter.position;
+            projectile.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg);
+            yield return intervalTime;
+        }
     }
 
 }

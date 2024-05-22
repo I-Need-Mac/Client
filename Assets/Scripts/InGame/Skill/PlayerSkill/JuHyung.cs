95c1589070030ca4285bac7944243241a9de0049
TEN3T <esther78944@gmail.com>
2024-05-23 02:05:51 +0900

    Feat: JuHyung update
    +스태틱 구현(이펙트 제외)

--- Assets/Scripts/InGame/Skill/PlayerSkill/JuHyung.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/JuHyung.cs
@@ -10,15 +10,56 @@ public class JuHyung : ActiveSkill
 
     public override IEnumerator Activation()
     {
-        for (int i = 0; i < skillData.projectileCount; i++)
+        //for (int i = 0; i < skillData.projectileCount; i++)
+        //{
+        //    Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
+        //    projectile.transform.localPosition = shooter.position;
+        //    Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
+        //    pos -= (Vector2)shooter.position;
+        //    projectile.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - 90.0f);
+        //    yield return intervalTime;
+        //}
+        Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
+        SkillManager.Instance.CoroutineStarter(Chaining(projectile));
+        yield return intervalTime;
+    }
+
+    //ProjectileCount: 전이 개수
+    private IEnumerator Chaining(Projectile projectile)
+    {
+        //첫 전이 대상 몬스터
+        Transform firstTarget = Scanner.GetTargetTransform(skillData.skillTarget, shooter, skillData.attackDistance);
+        if (firstTarget == null)
         {
-            ProjectileStraight projectile = SkillManager.Instance.SpawnProjectile<ProjectileStraight>(skillData);
-            projectile.transform.localPosition = shooter.position;
-            Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
-            pos -= (Vector2)shooter.position;
-            projectile.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - 90.0f);
-            yield return intervalTime;
+            yield break;
         }
+
+        List<Transform> aeTarget = Scanner.RangeTarget(firstTarget, skillData.attackDistance * 0.5f, (int)LayerConstant.MONSTER);
+        if (aeTarget.Count < 1)
+        {
+            yield break;
+        }
+
+        Dictionary<Transform, float> map = new Dictionary<Transform, float>();      //key: 몬스터, value: 거리
+        foreach (Transform target in aeTarget)
+        {
+            map.Add(target, Vector2.Distance(firstTarget.position, target.position));
+        }
+        aeTarget = map.OrderBy(entry => entry.Value)
+                      .Select(entry => entry.Key)
+                      .ToList()
+                      .Take(skillData.projectileCount)
+                      .ToList();
+
+        foreach (Transform target in aeTarget)
+        {
+            if (target.TryGetComponent(out Monster monster))
+            {
+                monster.Hit(skillData.damage);
+            }
+        }
+
+        yield return frame;
     }
 
 }

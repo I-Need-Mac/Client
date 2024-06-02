2641f4a2de0cd71b12d2229a8a3d90c0d0dd99a6
TEN3T <esther78944@gmail.com>
2024-06-02 22:46:29 +0900

    Feat: JuHyung update

--- Assets/Scripts/InGame/Skill/PlayerSkill/JuHyung.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/JuHyung.cs
@@ -3,23 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class JuHyung : ActiveSkill
 {
+    private LineRenderer effect;
+
     public JuHyung(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
 
     public override IEnumerator Activation()
     {
-        //for (int i = 0; i < skillData.projectileCount; i++)
-        //{
-        //    Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
-        //    projectile.transform.localPosition = shooter.position;
-        //    Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
-        //    pos -= (Vector2)shooter.position;
-        //    projectile.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - 90.0f);
-        //    yield return intervalTime;
-        //}
         Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
+        effect = projectile.GetComponent<LineRenderer>();
+        effect.enabled = false;
+        effect.positionCount = 0;
+        projectile.transform.position = shooter.position;
         SkillManager.Instance.CoroutineStarter(Chaining(projectile));
         yield return intervalTime;
     }
@@ -27,14 +25,21 @@ public class JuHyung : ActiveSkill
     //ProjectileCount: 전이 개수
     private IEnumerator Chaining(Projectile projectile)
     {
+        List<Vector3> effectPos = new List<Vector3>()
+        {
+            shooter.position,
+        };
+
         //첫 전이 대상 몬스터
         Transform firstTarget = Scanner.GetTargetTransform(skillData.skillTarget, shooter, skillData.attackDistance);
         if (firstTarget == null)
         {
+            SkillManager.Instance.DeSpawnProjectile(projectile);
             yield break;
         }
+        effectPos.Add(firstTarget.position);
 
-        List<Transform> aeTarget = Scanner.RangeTarget(firstTarget, skillData.attackDistance * 0.5f, (int)LayerConstant.MONSTER);
+        List<Transform> aeTarget = Scanner.RangeTarget(firstTarget, skillData.splashRange, (int)LayerConstant.MONSTER);
         if (aeTarget.Count < 1)
         {
             yield break;
@@ -47,19 +52,45 @@ public class JuHyung : ActiveSkill
         }
         aeTarget = map.OrderBy(entry => entry.Value)
                       .Select(entry => entry.Key)
-                      .ToList()
                       .Take(skillData.projectileCount)
                       .ToList();
 
-        foreach (Transform target in aeTarget)
+        int count = aeTarget.Count < skillData.projectileCount ? aeTarget.Count : skillData.projectileCount;
+        for (int i = 0; i < count; i++)
         {
-            if (target.TryGetComponent(out Monster monster))
+            if (aeTarget[i].TryGetComponent(out Monster monster))
             {
-                monster.Hit(skillData.damage);
+                effectPos.Add(monster.transform.position);
+                monster.Hit(projectile.CalTotalDamage(monster));
             }
         }
 
-        yield return frame;
+        effect.positionCount = effectPos.Count;
+        effect.SetPositions(effectPos.ToArray());
+        effect.enabled = true;
+
+        yield return new WaitForSeconds(1.0f);
+        SkillManager.Instance.DeSpawnProjectile(projectile);
     }
+
+    //private IEnumerator Move(Projectile projectile, Transform target, float speed = 15.0f)
+    //{
+    //    projectile.GetComponent<TrailRenderer>().enabled = true;
+    //    Vector2 direction = (target.position - projectile.transform.position).normalized;
+    //    float distance = 0.0f;
+    //    while (Vector2.Distance(projectile.transform.position, target.position) > 0.2f)
+    //    {
+    //        float shift = Time.fixedDeltaTime * skillData.speed * speed;
+    //        distance += shift;
+    //        if (distance >= skillData.attackDistance * 1.1f || !target.gameObject.activeInHierarchy)
+    //        {
+    //            break;
+    //        }
+    //        projectile.transform.Translate(direction * shift);
+    //        yield return frame;
+    //    }
+
+    //    SkillManager.Instance.DeSpawnProjectile(projectile);
+    //}
 
 }

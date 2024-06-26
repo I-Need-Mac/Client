4d6f0c889e1716c1418552770cafebb1fceae869
TEN3T <esther78944@gmail.com>
2024-06-26 21:30:37 +0900

    Feat: Ildo effect update

--- Assets/Scripts/InGame/Skill/PlayerSkill/Ildo.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Ildo.cs
@@ -8,19 +8,6 @@ public class Ildo : ActiveSkill
     public Ildo(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
     public override IEnumerator Activation()
     {
-        //Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
-        //projectile.transform.localPosition = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
-        //projectile.CollisionPower(false);     
-        //int padongCount = 0;
-        //do
-        //{
-        //    yield return intervalTime;
-        //    SkillManager.Instance.CoroutineStarter(Padong(projectile));
-        //    padongCount++;
-        //} while (padongCount < skillData.projectileCount);
-        //yield return frame;
-        //SkillManager.Instance.DeSpawnProjectile(projectile);
-
         for (int i = 0; i < skillData.projectileCount; i++)
         {
             Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
@@ -34,15 +21,20 @@ public class Ildo : ActiveSkill
 
     private IEnumerator Effect(Projectile projectile)
     {
+        ParticleSystem effect = projectile.GetComponentInChildren<ParticleSystem>();
+        effect.Play();
+
         Vector2 targetPos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
         Vector2 direction = (targetPos - (Vector2)shooter.position).normalized;
         Vector3 speed = direction * skillData.speed * Time.fixedDeltaTime;
+
         projectile.transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+
         float diff = 0.0f;
         float distance = Vector2.Distance(projectile.transform.position, targetPos);
+
         do
         {
-            //projectile.transform.position = Vector3.MoveTowards(projectile.transform.position, targetPos, skillData.speed * Time.fixedDeltaTime);
             projectile.transform.position = Vector3.SmoothDamp(projectile.transform.position, targetPos, ref speed, 0.25f);
             yield return frame;
             diff = Vector2.Distance(projectile.transform.position, targetPos);
@@ -52,6 +44,7 @@ public class Ildo : ActiveSkill
             }
         } while (diff > 0.25f);
 
+        effect.Clear();
         projectile.SetAlpha(1.0f);
         projectile.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
         projectile.transform.position = targetPos + new Vector2(0.0f, 1.5f);
@@ -67,18 +60,4 @@ public class Ildo : ActiveSkill
         yield return new WaitForSeconds(0.5f);
         SkillManager.Instance.DeSpawnProjectile(projectile);
     }
-
-    //private IEnumerator Padong(Projectile projectile)
-    //{
-    //    Collider2D[] hits = Physics2D.OverlapCircleAll(projectile.transform.position, skillData.splashRange);
-    //    foreach(Collider2D target in hits)
-    //    {
-    //        if(target.TryGetComponent(out Monster monster))
-    //        {
-    //            monster.SkillEffectActivation(skillData.skillEffect[1], float.Parse(skillData.skillEffectParam[1]));
-    //            monster.Hit(skillData.damage);
-    //        }
-    //    }
-    //    yield return frame;
-    //}
 }

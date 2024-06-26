206fab906d922eaa769f03a3a1074e29e5fb26a9
TEN3T <esther78944@gmail.com>
2024-06-26 20:09:44 +0900

    Feat: HwiPung effect update

--- Assets/Scripts/InGame/Skill/Projectile.cs
+++ Assets/Scripts/InGame/Skill/Projectile.cs
@@ -57,9 +57,28 @@ public class Projectile : MonoBehaviour
             }
         }
 
+        SetSortingLayer(transform.Find("Effect"));
+
         if (TryGetComponent(out Animator animator))
         {
             this.animator = animator;
+        }
+    }
+
+    private void SetSortingLayer(Transform trans)
+    {
+        if (trans == null)
+        {
+            return;
+        }
+        if (trans.TryGetComponent(out Renderer renderer))
+        {
+            renderer.sortingLayerName = LayerConstant.SPAWNOBJECT.ToString();
+        }
+
+        foreach (Transform child in trans)
+        {
+            SetSortingLayer(child);
         }
     }
 

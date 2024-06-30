33f6767b87bc2bb07d92be2ce0dd5c3abc522fde
TEN3T <esther78944@gmail.com>
2024-06-30 17:14:16 +0900

    Feat: Crepitus refactoring

--- Assets/Scripts/InGame/Skill/PlayerSkill/Crepitus.cs
+++ Assets/Scripts/InGame/Skill/PlayerSkill/Crepitus.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class Crepitus : ActiveSkill
 {
-    private WaitForSeconds tick;
-    private float size;
+    //private float size;
 
     public Crepitus(int skillId, Transform shooter, int skillNum) : base(skillId, shooter, skillNum) { }
 
     public override IEnumerator Activation()
     {
-        tick = new WaitForSeconds(0.25f);
-        size = skillData.attackDistance >= 10 ? 10 : skillData.attackDistance;
+        //size = skillData.attackDistance >= 10 ? 10 : skillData.attackDistance;
 
         for (int i = 0; i < skillData.projectileCount; i++)
         {
-            Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
-            SkillManager.Instance.CoroutineStarter(Boom(projectile));
+            Vector2 pos = Scanner.GetTarget(skillData.skillTarget, shooter, skillData.attackDistance);
+            if (pos == Vector2.one)
+            {
+                continue;
+            }
+            if (CameraManager.Instance.IsTargetVisible(pos))
+            {
+                Projectile projectile = SkillManager.Instance.SpawnProjectile<Projectile>(skillData);
+                projectile.transform.localScale = Vector3.one * skillData.splashRange;
+                projectile.transform.position = pos;
+                SkillManager.Instance.CoroutineStarter(Boom(projectile));
+            }
+            
             yield return intervalTime;
         }
     }
 
     private IEnumerator Boom(Projectile projectile)
     {
-        projectile.transform.localPosition = Scanner.GetTarget(SKILLCONSTANT.SKILL_TARGET.RANDOM, shooter, size);
-        yield return tick;
+        yield return new WaitForSeconds(3.0f);
+        foreach(Transform t in Scanner.RangeTarget(projectile.transform, skillData.splashRange, (int)LayerConstant.MONSTER))
+        {
+            if (t.TryGetComponent(out Monster monster))
+            {
+                monster.Hit(GameManager.Instance.player.playerManager.TotalDamage(skillData.damage));
+            }
+        }
         SkillManager.Instance.DeSpawnProjectile(projectile);
     }
 }
